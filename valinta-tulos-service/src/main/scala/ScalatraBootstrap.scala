96d847c7ddb278e5cbe82f9954589d8071ea5f44
Jani Rahkola <jani.rahkola@reaktor.com>
2016-12-14 14:43:28 +0200

    Persistent sessions

--- valinta-tulos-service/src/main/scala/ScalatraBootstrap.scala
+++ valinta-tulos-service/src/main/scala/ScalatraBootstrap.scala
@@ -1,7 +1,7 @@
 import java.util
 import javax.servlet.{DispatcherType, ServletContext}
 
-import fi.vm.sade.security.{AuthenticationFilter, CasLogin}
+import fi.vm.sade.security.CasLogin
 import fi.vm.sade.valintatulosservice._
 import fi.vm.sade.valintatulosservice.config.{OhjausparametritAppConfig, VtsAppConfig}
 import fi.vm.sade.valintatulosservice.config.VtsAppConfig.{Dev, IT, VtsAppConfig}
@@ -74,14 +74,15 @@ class ScalatraBootstrap extends LifeCycle {
       context.mount(new KelaServlet(new KelaService(HakijaResolver(appConfig), hakuService, organisaatioService, valintarekisteriDb)), "/cas/kela")
 
 
-      context.addFilter("auth", new AuthenticationFilter(s"${appConfig.settings.securitySettings.casUrl}/login?service=${appConfig.securityContext.casServiceIdentifier}"))
-        .addMappingForUrlPatterns(util.EnumSet.allOf(classOf[DispatcherType]), true, "/auth/*")
+      val loginUrl = s"${appConfig.settings.securitySettings.casUrl}/login?service=${appConfig.securityContext.casServiceIdentifier}"
       context.mount(new CasLogin(
         appConfig.securityContext.casClient,
         appConfig.securityContext.casServiceIdentifier,
-        appConfig.securityContext.directoryClient
+        appConfig.securityContext.directoryClient,
+        loginUrl,
+        valintarekisteriDb
       ), "/auth/login")
-      context.mount(new ValinnanTulosServlet(ilmoittautumisService), "/auth/valinnan-tulos")
+      context.mount(new ValinnanTulosServlet(ilmoittautumisService, valintarekisteriDb), "/auth/valinnan-tulos")
     }
     context.mount(new HakukohdeRefreshServlet(valintarekisteriDb, hakukohdeRecordService), "/virkistys")
 
