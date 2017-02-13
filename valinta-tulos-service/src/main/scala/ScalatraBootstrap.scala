07329a2954657f6dbccecae1462b17136f2f6db4
Jessika Penttinen <jessika.penttinen@siili.com>
2017-02-13 14:18:49 +0200

    Lisätty hakukohteen tallennus erillishaun valinnantulosten tallennukseen

--- valinta-tulos-service/src/main/scala/ScalatraBootstrap.scala
+++ valinta-tulos-service/src/main/scala/ScalatraBootstrap.scala
@@ -59,7 +59,7 @@ class ScalatraBootstrap extends LifeCycle {
     lazy val valintatulosCollection = new ValintatulosMongoCollection(appConfig.settings.valintatulosMongoConfig)
     lazy val mailPoller = new MailPoller(valintatulosCollection, valintatulosService, valintarekisteriDb, hakuService, appConfig.ohjausparametritService, limit = 100)
     lazy val sijoitteluService = new ValintarekisteriService(valintarekisteriDb, hakukohdeRecordService)
-    lazy val valinnantulosService = new ValinnantulosService(valintarekisteriDb, new OrganizationHierarchyAuthorizer(appConfig), hakuService, appConfig.ohjausparametritService, appConfig, audit)
+    lazy val valinnantulosService = new ValinnantulosService(valintarekisteriDb, new OrganizationHierarchyAuthorizer(appConfig), hakuService, appConfig.ohjausparametritService, hakukohdeRecordService, appConfig, audit)
 
 
     val migrationMode = System.getProperty("valinta-rekisteri-migration-mode")
