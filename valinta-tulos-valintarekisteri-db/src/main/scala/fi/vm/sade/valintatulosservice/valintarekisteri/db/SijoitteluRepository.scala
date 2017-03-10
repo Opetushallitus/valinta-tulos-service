f04791d0f7584ab4c77f4e02fb70387faccb2aee
Jessika Penttinen <jessika.penttinen@siili.com>
2017-03-10 13:10:22 +0200

    Implementoitu hakukohteen sijoittelun tulosten haku sekä refaktoroitu koodia

--- valinta-tulos-valintarekisteri-db/src/main/scala/fi/vm/sade/valintatulosservice/valintarekisteri/db/SijoitteluRepository.scala
+++ valinta-tulos-valintarekisteri-db/src/main/scala/fi/vm/sade/valintatulosservice/valintarekisteri/db/SijoitteluRepository.scala
@@ -3,10 +3,20 @@ package fi.vm.sade.valintatulosservice.valintarekisteri.db
 import fi.vm.sade.valintatulosservice.valintarekisteri.db.impl.ValintarekisteriRepository
 import fi.vm.sade.valintatulosservice.valintarekisteri.domain._
 
+import scala.util.Try
+
 trait SijoitteluRepository extends ValintarekisteriRepository {
   def storeSijoittelu(sijoittelu:SijoitteluWrapper)
 
   def getLatestSijoitteluajoId(hakuOid:String): Option[Long]
+
+  def getLatestSijoitteluajoId(sijoitteluajoId:String, hakuOid:String): Either[Throwable,Long] = sijoitteluajoId match {
+      case x if "latest".equalsIgnoreCase(x) => getLatestSijoitteluajoId(hakuOid).toRight(
+        new IllegalArgumentException(s"Yhtään sijoitteluajoa ei löytynyt haulle $hakuOid"))
+      case x => Try(x.toLong).toOption.toRight(
+        new IllegalArgumentException(s"Väärän tyyppinen sijoitteluajon ID: $sijoitteluajoId"))
+  }
+
   def getSijoitteluajo(sijoitteluajoId:Long): Option[SijoitteluajoRecord]
   def getSijoitteluajonHakukohteet(sijoitteluajoId:Long): List[SijoittelunHakukohdeRecord]
   def getSijoitteluajonValintatapajonot(sijoitteluajoId:Long): List[ValintatapajonoRecord]
@@ -23,6 +33,13 @@ trait SijoitteluRepository extends ValintarekisteriRepository {
   def getHakemuksenHakija(hakemusOid:String, sijoitteluajoId:Long): Option[HakijaRecord]
   def getHakemuksenHakutoiveet(hakemusOid:String, sijoitteluajoId:Long): List[HakutoiveRecord]
   def getHakemuksenPistetiedot(hakemusOid:String, sijoitteluajoId:Long): List[PistetietoRecord]
+
+  def getSijoitteluajonHakukohde(sijoitteluajoId:Long, hakukohdeOid:String): Option[SijoittelunHakukohdeRecord]
+  def getHakukohteenHakijaryhmat(sijoitteluajoId:Long, hakukohdeOid:String): List[HakijaryhmaRecord]
+  def getHakukohteenValintatapajonot(sijoitteluajoId:Long, hakukohdeOid:String): List[ValintatapajonoRecord]
+  def getHakukohteenPistetiedot(sijoitteluajoId:Long, hakukohdeOid:String): List[PistetietoRecord]
+  def getHakukohteenTilahistoriat(sijoitteluajoId:Long, hakukohdeOid:String): List[TilaHistoriaRecord]
+  def getHakukohteenHakemukset(sijoitteluajoId:Long, hakukohdeOid:String): List[HakemusRecord]
 
   def deleteSijoittelunTulokset(hakuOid: String): Unit
 }
