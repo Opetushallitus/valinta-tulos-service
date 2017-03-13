14b485097a6b8857c7460c30734833ca947b6c1d
Jessika Penttinen <jessika.penttinen@siili.com>
2017-03-13 10:13:59 +0200

    Lisätty testit hakukohteen sijoittelun tulosten haulle

--- valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/local/SijoitteluServletSpec.scala
+++ valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/local/SijoitteluServletSpec.scala
@@ -1,11 +1,15 @@
 package fi.vm.sade.valintatulosservice.local
 
 import fi.vm.sade.valintatulosservice._
+import fi.vm.sade.valintatulosservice.config.VtsAppConfig
 import fi.vm.sade.valintatulosservice.valintarekisteri.ValintarekisteriDbTools
-import org.json4s.DefaultFormats
+import org.json4s.{DefaultFormats, JArray}
 import org.json4s.jackson.JsonMethods
 import org.junit.runner.RunWith
+import org.mockserver.integration.ClientAndServer
+import org.mockserver.model.{HttpRequest, HttpResponse}
 import org.specs2.runner.JUnitRunner
+
 
 @RunWith(classOf[JUnitRunner])
 class SijoitteluServletSpec extends ServletSpecification with ValintarekisteriDbTools {
@@ -13,6 +17,16 @@ class SijoitteluServletSpec extends ServletSpecification with ValintarekisteriDb
   step(singleConnectionValintarekisteriDb.storeSijoittelu(loadSijoitteluFromFixture("haku-1.2.246.562.29.75203638285", "QA-import/")))
 
   lazy val testSession = createTestSession()
+
+  val organisaatioService:ClientAndServer = ClientAndServer.startClientAndServer(VtsAppConfig.organisaatioMockPort)
+
+  organisaatioService.when(new HttpRequest().withPath(
+    s"/organisaatio-service/rest/organisaatio/${appConfig.settings.rootOrganisaatioOid}/parentoids"
+  )).respond(new HttpResponse().withStatusCode(200).withBody("1.2.246.562.10.00000000001"))
+
+  organisaatioService.when(new HttpRequest().withPath(
+    s"/organisaatio-service/rest/organisaatio/123.123.123.123/parentoids"
+  )).respond(new HttpResponse().withStatusCode(200).withBody("1.2.246.562.10.00000000001/1.2.246.562.10.39804091914/123.123.123.123"))
 
   "GET /sijoittelu/:hakuOid/sijoitteluajo/:sijoitteluajoOid" should {
     "Hakee sijoittelun" in {
@@ -34,5 +48,24 @@ class SijoitteluServletSpec extends ServletSpecification with ValintarekisteriDb
     }
   }
 
+  "GET /auth/sijoittelu/:hakuOid/sijoitteluajo/:sijoitteluajoId/hakukohde/:hakukohdeOid" should {
+    "Hakee hakukohteen sijoittelun tuloksen" in {
+      get("auth/sijoittelu/1.2.246.562.29.75203638285/sijoitteluajo/1476936450191/hakukohde/1.2.246.562.20.26643418986", Seq.empty, Map("Cookie" -> s"session=${testSession}")) {
+        status must_== 200
+
+        val hakukohdeJson = JsonMethods.parse(body)
+
+        (hakukohdeJson \ "oid").extract[String] mustEqual "1.2.246.562.20.26643418986"
+        (hakukohdeJson \ "hakijaryhmat").asInstanceOf[JArray].arr.size mustEqual 2
+
+        val valintatapajonot = (hakukohdeJson \ "valintatapajonot").asInstanceOf[JArray]
+
+        valintatapajonot.arr.size mustEqual 1
+        (valintatapajonot(0) \ "hakemukset").asInstanceOf[JArray].arr.size mustEqual 15
+      }
+    }
+  }
+
+  step(organisaatioService.stop())
   step(deleteAll)
 }
