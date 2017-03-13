cc7913986913a62f65e00eb5d61d41bc422f9068
Jani Rahkola <jani.rahkola@reaktor.com>
2017-03-13 12:56:53 +0200

    Remove dead code

--- valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/VastaanotettavuusService.scala
+++ valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/VastaanotettavuusService.scala
@@ -1,7 +1,7 @@
 package fi.vm.sade.valintatulosservice
 
 import fi.vm.sade.valintatulosservice.valintarekisteri.db.{HakijaVastaanottoRepository, VastaanottoRecord}
-import fi.vm.sade.valintatulosservice.valintarekisteri.domain.{HakukohdeRecord, PriorAcceptanceException}
+import fi.vm.sade.valintatulosservice.valintarekisteri.domain.HakukohdeRecord
 import fi.vm.sade.valintatulosservice.valintarekisteri.hakukohde.HakukohdeRecordService
 import slick.dbio.{DBIO, DBIOAction}
 
@@ -9,7 +9,6 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 class VastaanotettavuusService(hakukohdeRecordService: HakukohdeRecordService,
                                hakijaVastaanottoRepository: HakijaVastaanottoRepository) {
-  private val defaultPriorAcceptanceHandler: VastaanottoRecord => DBIO[Unit] = aiempiVastaanotto => DBIOAction.failed(PriorAcceptanceException(aiempiVastaanotto))
 
   def tarkistaAiemmatVastaanotot(henkiloOid: String, hakukohdeOid: String, priorAcceptanceHandler: VastaanottoRecord => DBIO[Unit]): DBIO[Unit] = {
     val hakukohdeRecord = hakukohdeRecordService.getHakukohdeRecord(hakukohdeOid) match {
@@ -21,8 +20,6 @@ class VastaanotettavuusService(hakukohdeRecordService: HakukohdeRecordService,
       case Some(aiempiVastaanotto) => priorAcceptanceHandler(aiempiVastaanotto)
     }
   }
-
-  def tarkistaAiemmatVastaanotot(henkiloOid: String, hakukohdeOid: String): DBIO[Unit] = tarkistaAiemmatVastaanotot(henkiloOid, hakukohdeOid, defaultPriorAcceptanceHandler)
 
   private def haeAiemmatVastaanotot(hakukohdeRecord: HakukohdeRecord, hakijaOid: String): DBIO[Option[VastaanottoRecord]] = {
     val HakukohdeRecord(hakukohdeOid, _, yhdenPaikanSaantoVoimassa, _, koulutuksenAlkamiskausi) = hakukohdeRecord
