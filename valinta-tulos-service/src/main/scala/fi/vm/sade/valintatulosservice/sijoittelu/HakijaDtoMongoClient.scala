b6227ec53b609a8a375af2e59f58973874a0382e
Tommi Helander <tommi.helander@gofore.com>
2017-03-14 12:54:23 +0200

    Add logging to HakijaDtoMongoClient

--- valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/sijoittelu/HakijaDtoMongoClient.scala
+++ valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/sijoittelu/HakijaDtoMongoClient.scala
@@ -7,18 +7,27 @@ import java.util.function.Consumer
 import fi.vm.sade.sijoittelu.tulos.dto.raportointi.{HakijaDTO, HakijaPaginationObject}
 import fi.vm.sade.valintatulosservice.config.VtsAppConfig.VtsAppConfig
 import fi.vm.sade.sijoittelu.domain.SijoitteluAjo
+import fi.vm.sade.utils.slf4j.Logging
 /**
   * Fetches HakijaDTOs directly from mongodb database
   */
-class HakijaDtoMongoClient(appConfig: VtsAppConfig) extends StreamingHakijaDtoClient(appConfig) {
+class HakijaDtoMongoClient(appConfig: VtsAppConfig) extends StreamingHakijaDtoClient(appConfig) with Logging {
   private val raportointiService = appConfig.sijoitteluContext.raportointiService
 
   override def processSijoittelunTulokset[T](hakuOid: String, sijoitteluajoId: String, processor: HakijaDTO => T): Unit = {
     val sijoitteluAjo: Optional[SijoitteluAjo] = getSijoitteluAjo(sijoitteluajoId, hakuOid)
-    val hakijaPaginationObj: HakijaPaginationObject = raportointiService.hakemukset(sijoitteluAjo.get(), null, null, null, null, null, null)
-    val hakijat: util.List[HakijaDTO] = hakijaPaginationObj.getResults
+    val hakijat: util.List[HakijaDTO] = raportointiService
+      .hakemukset(sijoitteluAjo.get(), null, null, null, null, null, null).getResults
+
+    var count: Int = 0
     hakijat.forEach(new Consumer[HakijaDTO] {
-      override def accept(t: HakijaDTO): Unit = processor(t)
+      override def accept(t: HakijaDTO): Unit = {
+        count += 1
+        processor(t)
+        if (count % 1000 == 0) {
+          logger.info(s"...processed $count items so far...")
+        }
+      }
     })
   }
 
