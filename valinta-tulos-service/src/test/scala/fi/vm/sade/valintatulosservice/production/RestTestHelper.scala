b07ca9a3ae8eacff688b08511a0d5aea7eb9def7
Jessika Penttinen <jessika.penttinen@siili.com>
2017-03-15 10:06:26 +0200

    Lisätty RestTest sijoittelun hakemukselle

---
+++ valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/production/RestTestHelper.scala
@@ -0,0 +1,102 @@
+package fi.vm.sade.valintatulosservice.production
+
+import fi.vm.sade.utils.cas.{CasAuthenticatingClient, CasClient, CasParams}
+import fi.vm.sade.utils.http.DefaultHttpRequest
+import org.http4s.client.Client
+import org.http4s.{Method, Request, Uri}
+import org.json4s.DefaultFormats
+import org.json4s.native.JsonMethods.parse
+
+import scalaj.http.{Http, HttpOptions}
+import scalaz.concurrent.Task
+
+trait RestTestHelper {
+  val cas_user = System.getProperty("cas_user")
+  val cas_password = System.getProperty("cas_password")
+  def cas_url:String
+
+  lazy val vanhaSijoitteluCasClient = createCasClient("/sijoittelu-service")
+
+  implicit val formats = DefaultFormats
+
+  protected def getOld(uriString:String) = {
+    vanhaSijoitteluCasClient.prepare(Request(method = Method.GET, uri = createUri(uriString))).flatMap {
+      case r if 200 == r.status.code => r.as[String]
+      case r => Task.fail(new RuntimeException(s"$uriString => ${r.toString}"))
+    }.run
+  }
+
+  protected def createUri(uriString:String): Uri = Uri.fromString(uriString).getOrElse(throw new RuntimeException(s"Invalid uri"))
+
+  protected def createCasClient(target:String): Client = {
+    val casParams = CasParams(target, cas_user, cas_password)
+    new CasAuthenticatingClient(
+      new CasClient(cas_url, org.http4s.client.blaze.defaultClient),
+      casParams,
+      org.http4s.client.blaze.defaultClient,
+      null
+    )
+  }
+
+  protected def get[T](fetch:() => String)(implicit m: Manifest[T]): T = parse(fetch()).extract[T]
+
+  protected def getNew(url: String, vtsSessionCookie: String) = {
+    val (statusCode, responseHeaders, result) =
+      new DefaultHttpRequest(Http(url)
+        .method("GET")
+        .options(Seq(HttpOptions.connTimeout(10000), HttpOptions.readTimeout(120000)))
+        .header("Content-Type", "application/json")
+        .header("Cookie", s"session=$vtsSessionCookie")
+      ).responseWithHeaders()
+    if (statusCode != 200) {
+      throw new RuntimeException(s"Got status $statusCode from $url with headers $responseHeaders and body $result")
+    }
+    result
+  }
+}
+
+case class Sijoitteluajo(sijoitteluajoId:Long, hakuOid:String, startMils:Long, endMils:Long, hakukohteet:List[Hakukohde])
+
+case class Hakukohde(sijoitteluajoId:Option[Long], oid:String, tila:Option[String], tarjoajaOid:Option[String],
+                     kaikkiJonotSijoiteltu:Boolean, ensikertalaisuusHakijaryhmanAlimmatHyvaksytytPisteet:Option[Long],
+                     valintatapajonot:List[Valintatapajono], hakijaryhmat:List[Hakijaryhma])
+
+case class Hakijaryhma(prioriteetti:Option[Long], paikat:Option[Long], oid:String, nimi:Option[String],
+                       hakukohdeOid:Option[String], kiintio:Option[Long],
+                       kaytaKaikki:Option[Boolean], tarkkaKiintio:Option[Boolean], kaytetaanRyhmaanKuuluvia:Option[Boolean],
+                       hakijaryhmatyyppikoodiUri:Option[String], valintatapajonoOid:Option[String], hakemusOid:List[String])
+
+case class Valintatapajono(tasasijasaanto:Option[String], tila:Option[String], oid:String,
+                           prioriteetti:Option[Long], aloituspaikat:Option[Long], alkuperaisetAloituspaikat:Option[Long], alinHyvaksyttyPistemaara:Option[Long],
+                           eiVarasijatayttoa:Option[Boolean], kaikkiEhdonTayttavatHyvaksytaan:Option[Boolean], poissaOlevaTaytto:Option[Boolean], valintaesitysHyvaksytty:Option[Boolean],
+                           hakeneet:Option[Long], hyvaksytty:Option[Long], varalla:Option[Long], varasijat:Option[Long], hakemukset:List[Hakemus],
+                           varasijaTayttoPaivat:Option[java.util.Date], varasijojaTaytetaanAsti:Option[java.util.Date], tayttojono:Option[String])
+
+case class Hakemus(hakijaOid:Option[String], hakemusOid:String, pisteet:Option[Long], paasyJaSoveltuvuusKokeenTulos:Option[Long],
+                   etunimi:Option[String], sukunimi:Option[String], prioriteetti:Option[Long], jonosija:Option[Long], tasasijaJonosija:Option[Long],
+                   tila:Option[String], hyvaksyttyHarkinnanvaraisesti:Option[Boolean], varasijanNumero:Option[Long], sijoitteluajoId:Option[Long],
+                   hakukohdeOid:Option[String], tarjoajaOid:Option[String], valintatapajonoOid:Option[String],
+                   hakuOid:Option[String], onkoMuuttunutViimeSijoittelussa:Option[Boolean], siirtynytToisestaValintatapajonosta:Option[Boolean],
+                   tilanKuvaukset:Tilankuvaus, pistetiedot:List[Pistetieto], tilaHistoria:List[Tilahistoria])
+
+case class Pistetieto(tunniste:String, arvo:String, laskennallinenArvo:String, osallistuminen:String, tyypinKoodiUri:String, tilastoidaan:Boolean)
+
+case class Tilankuvaus(SV:Option[String], FI:Option[String], EN:Option[String])
+
+case class Tilahistoria(tila:String, luotu:Long)
+
+case class Hakija(hakijaOid:String, hakemusOid:String, etunimi:String, sukunimi:String, hakutoiveet:List[Hakutoive])
+
+case class Hakutoive(hakutoive:Int, hakukohdeOid:String, tarjoajaOid:Option[String], pistetiedot:List[Pistetieto], hakijaryhmat:List[Hakijaryhma],
+                     kaikkiJonotSijoiteltu:Option[Boolean], ensikertalaisuusHakijaryhmanAlimmatHyvaksytytPisteet:Option[String],
+                     vastaanottotieto:Option[String], hakutoiveenValintatapajonot:List[HakutoiveenValintatapajono])
+
+case class HakutoiveenValintatapajono(valintatapajonoOid:String, valintatapajonoNimi:String, eiVarasijatayttoa:Option[Boolean],
+                                      jonosija:Option[Int], paasyJaSoveltuvuusKokeenTulos:Option[String], varasijanNumero:Option[Int],
+                                      tila:Option[String], tilanKuvaukset:Tilankuvaus, ilmoittautumisTila:Option[String],
+                                      hyvaksyttyHarkinnanvaraisesti:Option[Boolean], tasasijaJonosija:Option[Int], pisteet:Option[String],
+                                      alinHyvaksyttyPistemaara:Option[String], hakeneet:Option[Int], hyvaksytty:Option[Int], varalla:Option[Int],
+                                      varasijat:Option[Int], varasijaTayttoPaivat:Option[Int], varasijojaKaytetaanAlkaen:Option[java.util.Date],
+                                      varasijojaTaytetaanAsti:Option[java.util.Date], valintatuloksenViimeisinMuutos:Option[java.util.Date],
+                                      hakemuksenTilanViimeisinMuutos:Option[java.util.Date], tayttojono:Option[String], julkaistavissa:Option[Boolean],
+                                      ehdollisestiHyvaksyttavissa:Option[Boolean], hyvaksyttyVarasijalta:Option[Boolean])
--- valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/production/SijoitteluRestTest.scala
+++ valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/production/SijoitteluRestTest.scala
@@ -3,15 +3,9 @@ package fi.vm.sade.valintatulosservice.production
 import java.io.File
 
 import fi.vm.sade.utils.cas._
-import fi.vm.sade.utils.http.DefaultHttpRequest
 import fi.vm.sade.utils.slf4j.Logging
 import fi.vm.sade.valintatulosservice.logging.PerformanceLogger
 import org.apache.commons.io.FileUtils
-import org.http4s.client.Client
-import org.http4s.dsl._
-import org.http4s.{Method, Request, Uri}
-import org.json4s.DefaultFormats
-import org.json4s.native.JsonMethods._
 import org.junit.Ignore
 import org.junit.runner.RunWith
 import org.specs2.matcher.MatcherMacros
@@ -19,21 +13,18 @@ import org.specs2.mutable.Specification
 import org.specs2.runner.JUnitRunner
 
 import scala.language.experimental.macros
-import scalaj.http.{Http, HttpOptions}
-import scalaz.concurrent.Task
-
 
 
 @Ignore
 @RunWith(classOf[JUnitRunner])
-class SijoitteluRestTest extends Specification with MatcherMacros with Logging with PerformanceLogger {
+class SijoitteluRestTest extends Specification with MatcherMacros with Logging with PerformanceLogger with RestTestHelper {
   val oldHost = "https://testi.virkailija.opintopolku.fi"
   //private val newHost = "https://testi.virkailija.opintopolku.fi"
   private val newHost = "http://localhost:8097"
 
-  val cas_user = System.getProperty("cas_user")
-  val cas_password = System.getProperty("cas_password")
-  val cas_url = oldHost + "/cas"
+  //val cas_user = System.getProperty("cas_user")
+  //val cas_password = System.getProperty("cas_password")
+  override val cas_url = oldHost + "/cas"
   //val haku_oid = "1.2.246.562.29.75203638285" // Kevään 2016 kk-yhteishaku
   //val haku_oid = "1.2.246.562.29.14662042044" // Kevään 2016 2. asteen yhteishaku
   //val haku_oid = "1.2.246.562.29.95390561488" // Kevään 2015 kk-yhteishaku
@@ -47,10 +38,6 @@ class SijoitteluRestTest extends Specification with MatcherMacros with Logging w
 
   def info(message:String) = if(infoOn) logger.info(message)
   def debug(message:String) = if(debugOn) logger.info(message)
-
-  implicit val formats = DefaultFormats
-
-  val vanhaSijoitteluCasClient = createCasClient("/sijoittelu-service")
 
   "New sijoittelu (valintarekisteri) and old sijoittelu (sijoitteluDb)" should {
     "contain same information" in {
@@ -216,78 +203,16 @@ class SijoitteluRestTest extends Specification with MatcherMacros with Logging w
     sijoitteluajo.copy(hakukohteet = sijoitteluajo.hakukohteet.map(h => get[Hakukohde](getHakukohde(hakuOid, h.oid))))
   }
 
-  private def get[T](fetch:() => String)(implicit m: Manifest[T]): T = parse(fetch()).extract[T]
-
   private def getSijoitteluajo(hakuOid: String): String = getOld(s"$oldHost/sijoittelu-service/resources/sijoittelu/$hakuOid/sijoitteluajo/latest")
   private def getHakukohde(hakuOid: String, hakukohdeOid: String) = () => getOld(s"$oldHost/sijoittelu-service/resources/sijoittelu/$hakuOid/sijoitteluajo/latest/hakukohde/$hakukohdeOid")
 
   private def getNewSijoittelu(hakuOid: String, vtsSessionCookie: String) = {
     val url = newHost + s"/valinta-tulos-service/auth/sijoittelu/$hakuOid/sijoitteluajo/latest"
-    val (statusCode, responseHeaders, result) = time("Uuden sijoittelun haku") {
-      new DefaultHttpRequest(Http(url)
-        .method("GET")
-        .options(Seq(HttpOptions.connTimeout(10000), HttpOptions.readTimeout(120000)))
-        .header("Content-Type", "application/json")
-        .header("Cookie", s"session=$vtsSessionCookie")
-      ).responseWithHeaders() }
-    if (statusCode != 200) {
-      throw new RuntimeException(s"Got status $statusCode from $url with headers $responseHeaders and body $result")
-    }
+    val result = time("Uuden sijoittelun haku") { getNew(url, vtsSessionCookie)}
     fileForStoringNewResponse.foreach { f =>
       info(s"Tallennetaan uuden APIn vastaus (${result.size} tavua) tiedostoon $f")
       FileUtils.writeStringToFile(new File(f), result)
     }
     result
   }
-
-
-  private def getOld(uriString:String) = {
-    vanhaSijoitteluCasClient.prepare(Request(method = Method.GET, uri = createUri(uriString))).flatMap {
-      case r if 200 == r.status.code => r.as[String]
-      case r => Task.fail(new RuntimeException(r.toString))
-    }.run
-  }
-
-  private def createUri(uriString:String): Uri = Uri.fromString(uriString).getOrElse(throw new RuntimeException(s"Invalid uri"))
-
-  private def createCasClient(target:String): Client = {
-    val casParams = CasParams(target, cas_user, cas_password)
-    new CasAuthenticatingClient(
-      new CasClient(cas_url, org.http4s.client.blaze.defaultClient),
-      casParams,
-      org.http4s.client.blaze.defaultClient,
-      null
-    )
-  }
-
-
 }
-case class Sijoitteluajo(sijoitteluajoId:Long, hakuOid:String, startMils:Long, endMils:Long, hakukohteet:List[Hakukohde])
-
-case class Hakukohde(sijoitteluajoId:Option[Long], oid:String, tila:Option[String], tarjoajaOid:Option[String],
-                     kaikkiJonotSijoiteltu:Boolean, ensikertalaisuusHakijaryhmanAlimmatHyvaksytytPisteet:Option[Long],
-                     valintatapajonot:List[Valintatapajono], hakijaryhmat:List[Hakijaryhma])
-
-case class Hakijaryhma(prioriteetti:Option[Long], paikat:Option[Long], oid:String, nimi:Option[String],
-                       hakukohdeOid:Option[String], kiintio:Option[Long],
-                       kaytaKaikki:Boolean, tarkkaKiintio:Boolean, kaytetaanRyhmaanKuuluvia:Boolean,
-                       hakijaryhmatyyppikoodiUri:Option[String], valintatapajonoOid:Option[String], hakemusOid:List[String])
-
-case class Valintatapajono(tasasijasaanto:Option[String], tila:Option[String], oid:String,
-                           prioriteetti:Option[Long], aloituspaikat:Option[Long], alkuperaisetAloituspaikat:Option[Long], alinHyvaksyttyPistemaara:Option[Long],
-                           eiVarasijatayttoa:Boolean, kaikkiEhdonTayttavatHyvaksytaan:Boolean, poissaOlevaTaytto:Boolean, valintaesitysHyvaksytty:Option[Boolean],
-                           hakeneet:Option[Long], hyvaksytty:Option[Long], varalla:Option[Long], varasijat:Option[Long], hakemukset:List[Hakemus],
-                           varasijaTayttoPaivat:Option[java.util.Date], varasijojaTaytetaanAsti:Option[java.util.Date], tayttojono:Option[String])
-
-case class Hakemus(hakijaOid:Option[String], hakemusOid:String, pisteet:Option[Long], paasyJaSoveltuvuusKokeenTulos:Option[Long],
-                   etunimi:Option[String], sukunimi:Option[String], prioriteetti:Option[Long], jonosija:Option[Long], tasasijaJonosija:Option[Long],
-                   tila:Option[String], hyvaksyttyHarkinnanvaraisesti:Boolean, varasijanNumero:Option[Long], sijoitteluajoId:Option[Long],
-                   hakukohdeOid:Option[String], tarjoajaOid:Option[String], valintatapajonoOid:Option[String],
-                   hakuOid:Option[String], onkoMuuttunutViimeSijoittelussa:Boolean, siirtynytToisestaValintatapajonosta:Boolean,
-                   tilanKuvaukset:Tilankuvaus, pistetiedot:List[Pistetieto], tilaHistoria:List[Tilahistoria])
-
-case class Pistetieto(tunniste:String, arvo:String, laskennallinenArvo:String, osallistuminen:String, tyypinKoodiUri:String, tilastoidaan:Boolean)
-
-case class Tilankuvaus(SV:Option[String], FI:Option[String], EN:Option[String])
-
-case class Tilahistoria(tila:String, luotu:Long)
