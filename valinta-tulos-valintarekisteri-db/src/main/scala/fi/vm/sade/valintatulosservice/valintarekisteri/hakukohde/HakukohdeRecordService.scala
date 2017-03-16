3c6a4a81e3e205103b7cc07efa6c61ffff8af221
Jani Rahkola <jani.rahkola@reaktor.com>
2017-03-16 14:38:30 +0200

    Inject HakuService to HakukohdeRecordService

--- valinta-tulos-valintarekisteri-db/src/main/scala/fi/vm/sade/valintatulosservice/valintarekisteri/hakukohde/HakukohdeRecordService.scala
+++ valinta-tulos-valintarekisteri-db/src/main/scala/fi/vm/sade/valintatulosservice/valintarekisteri/hakukohde/HakukohdeRecordService.scala
@@ -117,11 +117,3 @@ class HakukohdeRecordService(hakuService: HakuService, hakukohdeRepository: Haku
     case Right(x)#::rest => sequence(rest).right.map(x +: _)
   }
 }
-
-object HakukohdeRecordService {
-
-  def apply(valintarekisteriDb: ValintarekisteriDb, appConfig: AppConfig) = {
-    val hakuService = HakuService(appConfig.hakuServiceConfig)
-    new HakukohdeRecordService(hakuService, valintarekisteriDb, appConfig.settings.lenientTarjontaDataParsing)
-  }
-}
