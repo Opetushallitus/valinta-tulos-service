8f5ef3ab933e00e3392331a138e250ec264a52ea
Tommi Helander <tommi.helander@gofore.com>
2017-03-16 15:08:47 +0200

    Revert "Merge pull request #29 from Opetushallitus/BUG-1334_odw-hakemukset"
    This reverts commit 6804f9ec542963e1ec265136c66eae368b67de1c, reversing
    changes made to cbffa5512649addb1ce03d67214d82d2105bb523.

--- valinta-tulos-valintarekisteri-db/src/main/scala/fi/vm/sade/valintatulosservice/config/ValintarekisteriApplicationSettings.scala
+++ valinta-tulos-valintarekisteri-db/src/main/scala/fi/vm/sade/valintatulosservice/config/ValintarekisteriApplicationSettings.scala
@@ -2,18 +2,13 @@ package fi.vm.sade.valintatulosservice.config
 
 import com.typesafe.config.Config
 import fi.vm.sade.properties.OphProperties
-import fi.vm.sade.utils.slf4j.Logging
 import org.apache.commons.lang3.BooleanUtils
 
-import scala.util.Try
-
-abstract class ApplicationSettings(config: Config) extends fi.vm.sade.utils.config.ApplicationSettings(config)
-  with Logging {
+abstract class ApplicationSettings(config: Config) extends fi.vm.sade.utils.config.ApplicationSettings(config) {
   val organisaatioUrl = withConfig(_.getString("organisaatio-service.url"))
   val tarjontaUrl = withConfig(_.getString("tarjonta-service.url"))
   val valintaRekisteriDbConfig = withConfig(_.getConfig("valinta-tulos-service.valintarekisteri.db"))
   val lenientTarjontaDataParsing: Boolean = BooleanUtils.isTrue(withConfig(_.getBoolean("valinta-tulos-service.parseleniently.tarjonta")))
-  val hakuOidsToLoadDirectlyFromMongo: Set[String] = parseStringSet("valinta-tulos-service.directmongo.hakuoids")
   val ophUrlProperties: OphProperties
   protected def withConfig[T](operation: Config => T): T = {
     try {
@@ -23,13 +18,6 @@ abstract class ApplicationSettings(config: Config) extends fi.vm.sade.utils.conf
         System.err.println(s"Cannot instantiate ${classOf[ApplicationSettings]} : ${e.getMessage}")
         e.printStackTrace()
         throw e
-    }
-  }
-
-  private def parseStringSet(propertyName: String, default: Set[String] = Set()): Set[String] = {
-    Try(withConfig(_.getString(propertyName))).map(_.split(",").toSet).getOrElse {
-      logger.warn(s"""Could not read property "$propertyName", returning default value $default""")
-      default
     }
   }
 }
