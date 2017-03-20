2273d9e644ea05bb82298860cd18c955d806ab4c
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2017-03-20 12:35:59 +0200

    Assume nothing on koulutuksen laajuus format

--- valinta-tulos-valintarekisteri-db/src/main/scala/fi/vm/sade/valintatulosservice/tarjonta/KelaKoulutus.scala
+++ valinta-tulos-valintarekisteri-db/src/main/scala/fi/vm/sade/valintatulosservice/tarjonta/KelaKoulutus.scala
@@ -2,7 +2,7 @@ package fi.vm.sade.valintatulosservice.tarjonta
 
 import fi.vm.sade.valintatulosservice.tarjonta.KelaKoulutus.{Hammas, Ylempi, Alempi, Lääkis}
 
-trait KelaKoulutus extends KelaLaajuus with KelaTutkinnontaso
+case class KelaKoulutus(val tutkinnontaso: Option[String],val tutkinnonlaajuus1: String, val tutkinnonlaajuus2: Option[String]) extends KelaLaajuus with KelaTutkinnontaso
 
 trait KelaLaajuus {
   val tutkinnonlaajuus1: String
@@ -11,16 +11,58 @@ trait KelaLaajuus {
 trait KelaTutkinnontaso {
   val tutkinnontaso: Option[String]
 }
-private case class MuuTutkinto(val tutkinnontaso: Option[String] = None, val tutkinnonlaajuus1: String, val tutkinnonlaajuus2: Option[String] = None) extends KelaKoulutus
-private case class AlempiKKTutkinto(val tutkinnontaso: Option[String] = Some("050"), val tutkinnonlaajuus1: String, val tutkinnonlaajuus2: Option[String] = None) extends KelaKoulutus
-private case class AlempiYlempiKKTutkinto(val tutkinnontaso: Option[String] = Some("060"), val tutkinnonlaajuus1: String, val tutkinnonlaajuus2: Option[String] = None) extends KelaKoulutus
-private case class ErillinenYlempiKKTutkinto(val tutkinnontaso: Option[String] = Some("061"), val tutkinnonlaajuus1: String, val tutkinnonlaajuus2: Option[String] = None) extends KelaKoulutus
-private case class LääketieteenLisensiaatti(val tutkinnontaso: Option[String] = Some("070"), val tutkinnonlaajuus1: String, val tutkinnonlaajuus2: Option[String] = None) extends KelaKoulutus
-private case class HammaslääketieteenLisensiaatti(val tutkinnontaso: Option[String] = Some("071"), val tutkinnonlaajuus1: String, val tutkinnonlaajuus2: Option[String] = None) extends KelaKoulutus
+private object MuuTutkinto {
+  def apply(laajuus: Option[String]) = {
+    val (laajuus1, laajuus2) = KelaKoulutus.asLaajuus1AndLaajuus2(laajuus)
+    KelaKoulutus(tutkinnontaso = None, tutkinnonlaajuus1 = laajuus1, tutkinnonlaajuus2 = laajuus2)
+  }
+}
+private object AlempiKKTutkinto {
+  def apply(laajuus: Option[String]) = {
+    val (laajuus1, laajuus2) = KelaKoulutus.asLaajuus1AndLaajuus2(laajuus)
+    KelaKoulutus(tutkinnontaso = Some("050"), tutkinnonlaajuus1 = laajuus1, tutkinnonlaajuus2 = laajuus2)
+  }
+}
+private object AlempiYlempiKKTutkinto {
+  def apply(laajuus: Option[String]) = {
+    val (laajuus1, laajuus2) = KelaKoulutus.asLaajuus1AndLaajuus2(laajuus)
+    KelaKoulutus(tutkinnontaso = Some("060"), tutkinnonlaajuus1 = laajuus1, tutkinnonlaajuus2 = laajuus2)
+  }
+}
+private object ErillinenYlempiKKTutkinto {
+  def apply(laajuus: Option[String]) = {
+    val (laajuus1, laajuus2) = KelaKoulutus.asLaajuus1AndLaajuus2(laajuus)
+    KelaKoulutus(tutkinnontaso = Some("061"), tutkinnonlaajuus1 = laajuus1, tutkinnonlaajuus2 = laajuus2)
+  }
+}
+private object LääketieteenLisensiaatti {
+  def apply(laajuus: Option[String]) = {
+    val (laajuus1, laajuus2) = KelaKoulutus.asLaajuus1AndLaajuus2(laajuus)
+    KelaKoulutus(tutkinnontaso = Some("070"), tutkinnonlaajuus1 = laajuus1, tutkinnonlaajuus2 = laajuus2)
+  }
+}
+private object HammaslääketieteenLisensiaatti {
+  def apply(laajuus: Option[String]) = {
+    val (laajuus1, laajuus2) = KelaKoulutus.asLaajuus1AndLaajuus2(laajuus)
+    KelaKoulutus(tutkinnontaso = Some("071"), tutkinnonlaajuus1 = laajuus1, tutkinnonlaajuus2 = laajuus2)
+  }
+}
 
 object KelaKoulutus {
 
-  private trait Taso
+   def asLaajuus1AndLaajuus2(laajuus: Option[String]): (String,Option[String]) = {
+     val l = laajuus.map(_.split("\\+").toList).getOrElse(List())
+     l match {
+       case List(l1,l2) => (l1, Some(l2))
+       case List(l1) => (l1, None)
+       case List() => ("", None)
+       case _ => throw new RuntimeException("Unexpected laajuus format: " + laajuus)
+     }
+  }
+
+  private trait Taso {
+    val laajuusarvo: Option[String]
+  }
   private case class Lääkis(val laajuusarvo: Option[String]) extends Taso
   private case class Hammas(val laajuusarvo: Option[String]) extends Taso
   private case class Alempi(val laajuusarvo: Option[String]) extends Taso
@@ -35,18 +77,13 @@ object KelaKoulutus {
       case b: Ylempi => 4
       case m: Muu => 5
     } match {
-      case Ylempi(laajuus) :: Nil =>
-        Some(ErillinenYlempiKKTutkinto(tutkinnonlaajuus1 = laajuus.getOrElse("")))
+      case Ylempi(laajuus) :: Nil => Some(ErillinenYlempiKKTutkinto(laajuus))
       case Alempi(laajuus1) :: Ylempi(laajuus2) :: Nil =>
-        Some(AlempiYlempiKKTutkinto(tutkinnonlaajuus1 = laajuus1.getOrElse(""), tutkinnonlaajuus2 = laajuus2))
-      case Alempi(laajuus) :: Nil =>
-        Some(AlempiKKTutkinto(tutkinnonlaajuus1 = laajuus.getOrElse("")))
-      case Lääkis(laajuus) :: _ =>
-        Some(LääketieteenLisensiaatti(tutkinnonlaajuus1 = laajuus.getOrElse("")))
-      case Hammas(laajuus) :: _ =>
-        Some(HammaslääketieteenLisensiaatti(tutkinnonlaajuus1 = laajuus.getOrElse("")))
-      case Muu(laajuus) :: _ =>
-        Some(MuuTutkinto(tutkinnonlaajuus1 = laajuus.getOrElse("")))
+        Some(AlempiYlempiKKTutkinto(laajuus2.map(l2 => laajuus1.map(_ + "+" + l2).getOrElse(l2)).orElse(laajuus1)))
+      case Alempi(laajuus) :: Nil => Some(AlempiKKTutkinto(laajuus))
+      case Lääkis(laajuus) :: _ => Some(LääketieteenLisensiaatti(laajuus))
+      case Hammas(laajuus) :: _ => Some(HammaslääketieteenLisensiaatti(laajuus))
+      case Muu(laajuus) :: _ => Some(MuuTutkinto(laajuus))
       case _ =>
         None
     }
@@ -56,7 +93,7 @@ object KelaKoulutus {
     implicit class Regex(sc: StringContext) {
       def r = new util.matching.Regex(sc.parts.mkString, sc.parts.tail.map(_ => "x"): _*)
     }
-    val arvo = k.opintojenLaajuusarvo.map(_.arvo)
+    val arvo = k.opintojenLaajuusarvo.map(_.arvo).filter(_.trim.nonEmpty)
     k.koulutuskoodi match {
       case Some(koodi) =>
         koodi.arvo match {
