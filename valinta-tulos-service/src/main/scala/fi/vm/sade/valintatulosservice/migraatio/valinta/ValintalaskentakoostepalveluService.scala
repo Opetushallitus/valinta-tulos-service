a9a1726ee847a1a126bad4ca951c28cd0bc1bfcf
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2017-04-11 15:32:13 +0300

    OK-119 : Add 3 retries to kayttaaLaskentaa calls
    They seem to fail intermittently possibly do to some
    network error.

--- valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/migraatio/valinta/ValintalaskentakoostepalveluService.scala
+++ valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/migraatio/valinta/ValintalaskentakoostepalveluService.scala
@@ -3,6 +3,7 @@ package fi.vm.sade.valintatulosservice.migraatio.valinta
 import java.net.URLEncoder
 
 import fi.vm.sade.utils.cas.{CasAuthenticatingClient, CasParams}
+import fi.vm.sade.utils.slf4j.Logging
 import fi.vm.sade.valintatulosservice.config.VtsAppConfig.VtsAppConfig
 import org.http4s.{Method, Request, Uri}
 import org.json4s._
@@ -14,11 +15,28 @@ import scalaz.concurrent.Task
 /**
   * Created by heikki.honkanen on 15/03/2017.
   */
-class ValintalaskentakoostepalveluService (appConfig: VtsAppConfig) {
+class ValintalaskentakoostepalveluService(appConfig: VtsAppConfig) extends Logging {
   implicit val formats = DefaultFormats
   private val valintalaskentakoosteClient = createCasClient(appConfig, "/valintalaskentakoostepalvelu/")
+  private val retryBackoffMillis = Seq(50, 500, 5000)
 
   def hakukohdeUsesLaskenta(hakukohdeOid: String): Boolean = {
+    val url = appConfig.ophUrlProperties.url("valintalaskentakoostepalvelu.valintaperusteet.resource.hakukohde", hakukohdeOid)
+    retryBackoffMillis.foreach { backoffMillis =>
+      try {
+        logger.info(s"Calling $url to see if kohde $hakukohdeOid uses laskenta")
+        return callKoostepalveluForKayttaaLaskentaa(url, hakukohdeOid)
+      } catch {
+        case e: Exception =>
+          logger.error(e.getMessage + s" : retrying after $backoffMillis ms", e)
+          Thread.sleep(backoffMillis)
+      }
+    }
+    throw new RuntimeException(s"Could not determine whether hakukohde $hakukohdeOid uses laskenta with ${retryBackoffMillis.size} " +
+      s"attempts with intervals of $retryBackoffMillis ms")
+  }
+
+  private def callKoostepalveluForKayttaaLaskentaa(url: String, hakukohdeOid: String): Boolean = {
     implicit val hakukohdeResponseReader = new Reader[HakukohdeResponse] {
       override def read(v: JValue): HakukohdeResponse = {
         HakukohdeResponse((v \ "kayttaaValintalaskentaa").extract[Boolean])
@@ -27,7 +45,6 @@ class ValintalaskentakoostepalveluService (appConfig: VtsAppConfig) {
 
     implicit val hakukohdeResponseDecoder =  org.http4s.json4s.native.jsonOf[HakukohdeResponse]
 
-    val url = appConfig.ophUrlProperties.url("valintalaskentakoostepalvelu.valintaperusteet.resource.hakukohde", hakukohdeOid)
     Try(
       valintalaskentakoosteClient.prepare({
         Request(method = Method.GET, uri = createUri(url, ""))
