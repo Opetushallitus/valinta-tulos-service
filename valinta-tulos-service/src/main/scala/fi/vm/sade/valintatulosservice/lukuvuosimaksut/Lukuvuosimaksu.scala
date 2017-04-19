493d0207e8be7309d4d326ddbb8ef51e2fa07379
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2017-04-19 14:37:21 +0300

    spec tests to 'Lukuvuosimaksu'

--- valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/lukuvuosimaksut/Lukuvuosimaksu.scala
+++ valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/lukuvuosimaksut/Lukuvuosimaksu.scala
@@ -1,3 +1,19 @@
 package fi.vm.sade.valintatulosservice.lukuvuosimaksut
 
-case class Lukuvuosimaksu(personOid: String, hakukohdeOid: String)
+import java.util.Date
+
+import fi.vm.sade.valintatulosservice.lukuvuosimaksut.Maksuntila.Maksuntila
+
+object Maksuntila extends Enumeration {
+  type Maksuntila = Value
+  val maksettu = Value("MAKSETTU")
+  val maksamatta = Value("MAKSAMATTA")
+  val vapautettu = Value("VAPAUTETTU")
+
+}
+
+
+case class Lukuvuosimaksu(personOid: String, hakukohdeOid: String, maksuntila: Maksuntila, muokkaaja: String, luotu: Date)
+
+
+case class LukuvuosimaksuMuutos(personOid: String, hakukohdeOid: String, maksuntila: Maksuntila)
---
+++ valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/local/LukuvuosimaksuServletSpec.scala
@@ -0,0 +1,55 @@
+package fi.vm.sade.valintatulosservice.local
+
+import fi.vm.sade.valintatulosservice.{ValintatuloksenTilaSerializer, VastaanottoActionSerializer, IlmoittautumistilaSerializer, ServletSpecification}
+import fi.vm.sade.valintatulosservice.lukuvuosimaksut.{LukuvuosimaksuMuutos, Maksuntila, Lukuvuosimaksu}
+import fi.vm.sade.valintatulosservice.valintarekisteri.ValintarekisteriDbTools
+import org.json4s.DefaultFormats
+import org.json4s.ext.EnumNameSerializer
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
+import org.specs2.specification.BeforeExample
+import slick.driver.PostgresDriver.api._
+
+@RunWith(classOf[JUnitRunner])
+class LukuvuosimaksuServletSpec extends ServletSpecification with ValintarekisteriDbTools {
+  override implicit val formats = DefaultFormats ++ List(new TasasijasaantoSerializer, new ValinnantilaSerializer,
+    new DateSerializer, new TilankuvauksenTarkenneSerializer, new IlmoittautumistilaSerializer, new VastaanottoActionSerializer, new ValintatuloksenTilaSerializer,
+    new EnumNameSerializer(Maksuntila))
+
+  lazy val muutos = LukuvuosimaksuMuutos("1.2.3.personOid","1.2.3.hakukohdeOid",Maksuntila.vapautettu)
+  lazy val testSession = createTestSession()
+  lazy val headers = Map("Cookie" -> s"session=${testSession}", "Content-type" -> "application/json")
+
+  "POST /auth/lukuvuosimaksu" should {
+    "palauttaa 204 kun tallennus onnistuu" in {
+      post(s"auth/lukuvuosimaksu", muutosAsJson, headers) {
+        status must_== 204
+      }
+    }
+
+    "palauttaa tallennetut datat pyydettäessä" in {
+      get(s"auth/lukuvuosimaksu/${muutos.hakukohdeOid}", Nil, headers) {
+        status must_== 200
+        import org.json4s.native.JsonMethods._
+        val maksu = parse(body).extract[List[Lukuvuosimaksu]]
+
+        maksu.map(m => LukuvuosimaksuMuutos(m.personOid,m.hakukohdeOid,m.maksuntila)).head must_== muutos
+      }
+    }
+
+    "palauttaa 500 kun syötetty data on virheellistä" in {
+      post(s"auth/lukuvuosimaksu", """[]""".getBytes("UTF-8"), headers) {
+        status must_== 500
+      }
+    }
+
+  }
+
+  private def muutosAsJson = {
+    import org.json4s.native.Serialization.{write}
+    val json = write(List(muutos))
+
+    json.getBytes("UTF-8")
+  }
+
+}
