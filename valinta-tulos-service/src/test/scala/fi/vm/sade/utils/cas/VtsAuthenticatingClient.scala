262734d8960372bc2d9460a7d6554027786b7d0e
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2017-04-28 09:30:08 +0300

    Parametrise security uri suffix
    and fix logging

--- valinta-tulos-service/src/test/scala/fi/vm/sade/utils/cas/VtsAuthenticatingClient.scala
+++ valinta-tulos-service/src/test/scala/fi/vm/sade/utils/cas/VtsAuthenticatingClient.scala
@@ -2,22 +2,21 @@ package fi.vm.sade.utils.cas
 
 import java.util.concurrent.TimeUnit
 
-import org.http4s._
-
 import scala.concurrent.duration.Duration
 import scalaz.{-\/, \/-}
 
 class VtsAuthenticatingClient(virkailijaBaseUrlForCas: String,
                               relativeServiceUrl: String,
+                              securityUriSuffix: String,
                               casUser: String,
                               casPassword: String) extends Logging {
   private val client = org.http4s.client.blaze.defaultClient
   private val casAuthenticatingClient = new CasClient(virkailijaBaseUrlForCas, client)
-  private val params = CasParams(relativeServiceUrl, casUser, casPassword)
+  private val params = CasParams(relativeServiceUrl, securityUriSuffix, casUser, casPassword)
 
   def getVtsSession(virkailijaBaseUrlForService: String): String = {
-    val serviceUri = Uri.fromString(virkailijaBaseUrlForService + relativeServiceUrl + "/auth/login").toOption.get
-    logger.info(s"Retrieving CAS service ticket from $virkailijaBaseUrlForCas and then session cookie from $serviceUri")
+    logger.info(
+      s"""Retrieving CAS service ticket from $virkailijaBaseUrlForCas with security uri = ${params.service.securityUri}""")
     casAuthenticatingClient.fetchCasSession(params, sessionCookieName = "session").attemptRunFor(Duration(1, TimeUnit.MINUTES)) match {
       case \/-(cookie) => cookie
       case -\/(t) =>
