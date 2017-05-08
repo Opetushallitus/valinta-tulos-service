40f6f3d13d57028896e6314ef89ca54a7197916f
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2017-05-08 14:11:37 +0300

    Start adding valintarekisteri-based functionality
    WIP

--- valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/sijoittelu/valintarekisteriRaportointiService.scala
+++ valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/sijoittelu/valintarekisteriRaportointiService.scala
@@ -1,10 +1,16 @@
 package fi.vm.sade.valintatulosservice.sijoittelu
 
-import fi.vm.sade.sijoittelu.domain.SijoitteluAjo
+import java.util.Collections.sort
+
+import fi.vm.sade.sijoittelu.domain.{Hakukohde, SijoitteluAjo, Valintatulos}
+import fi.vm.sade.sijoittelu.tulos.dto.HakemuksenTila.VARASIJALTA_HYVAKSYTTY
 import fi.vm.sade.sijoittelu.tulos.dto.raportointi.{HakijaDTO, HakijaPaginationObject, KevytHakijaDTO}
+import fi.vm.sade.sijoittelu.tulos.dto.{HakemuksenTila, ValintatuloksenTila}
+import fi.vm.sade.sijoittelu.tulos.service.impl.comparators.HakijaDTOComparator
+import fi.vm.sade.sijoittelu.tulos.service.impl.converters.{RaportointiConverterImpl, SijoitteluTulosConverterImpl}
 import fi.vm.sade.valintatulosservice.SijoitteluService
 import fi.vm.sade.valintatulosservice.valintarekisteri.db.SijoitteluRepository
-import fi.vm.sade.valintatulosservice.valintarekisteri.domain.{HakemusOid, HakuOid, HakukohdeOid}
+import fi.vm.sade.valintatulosservice.valintarekisteri.domain.{HakemusOid, HakemusRecord, HakuOid, HakukohdeOid}
 
 import scala.util.Try
 
@@ -30,9 +36,19 @@ trait ValintarekisteriRaportointiService {
   def hakemuksetVainHakukohteenTietojenKanssa(sijoitteluAjo: SijoitteluAjo, hakukohdeOid:HakukohdeOid): List[KevytHakijaDTO]
 }
 
-class ValintarekisteriRaportointiServiceImpl(sijoitteluService: SijoitteluService, sijoitteluRepository: SijoitteluRepository) extends ValintarekisteriRaportointiService {
-  override def latestSijoitteluAjoForHaku(hakuOid: HakuOid): Option[SijoitteluAjo] =
-    sijoitteluRepository.getLatestSijoitteluajoId(hakuOid).flatMap(getSijoitteluAjo)
+class ValintarekisteriRaportointiServiceImpl(sijoitteluService: SijoitteluService, sijoitteluRepository: SijoitteluRepository,
+                                         valintatulosDao: ValintarekisteriValintatulosDao) extends ValintarekisteriRaportointiService {
+  private val sijoitteluTulosConverter = new SijoitteluTulosConverterImpl
+  private val raportointiConverter = new RaportointiConverterImpl
+
+
+  override def latestSijoitteluAjoForHaku(hakuOid: HakuOid): Option[SijoitteluAjo] = {
+    sijoitteluRepository.getLatestSijoitteluajoId(hakuOid) match {
+      case Some(realSijoitteluajoId) => getSijoitteluAjo(realSijoitteluajoId)
+      case None => Some(SyntheticSijoitteluAjoForHakusWithoutSijoittelu(hakuOid))
+    }
+  }
+
 
   override def hakemus(sijoitteluAjo: SijoitteluAjo, hakemusOid:HakemusOid): Option[HakijaDTO] =
     hakemus(HakuOid(sijoitteluAjo.getHakuOid), "" + sijoitteluAjo.getSijoitteluajoId, hakemusOid)
@@ -46,7 +62,13 @@ class ValintarekisteriRaportointiServiceImpl(sijoitteluService: SijoitteluServic
                           vastaanottaneet: Option[Boolean],
                           hakukohdeOids: Option[List[HakukohdeOid]],
                           count: Option[Int],
-                          index: Option[Int]):HakijaPaginationObject = ???
+                          index: Option[Int]): HakijaPaginationObject = {
+    val sijoitteluajonHakemukset: Seq[HakemusRecord] = sijoitteluService.sijoitteluRepository.getSijoitteluajonHakemukset(sijoitteluAjo.getSijoitteluajoId)
+    //val hakukohteet: java.util.List[Hakukohde] = hakukohdeDao.getHakukohdeForSijoitteluajo(ajo.getSijoitteluajoId())
+
+    val valintatulokset = valintatulosDao.loadValintatulokset(HakuOid(sijoitteluAjo.getHakuOid))
+    new HakijaPaginationObject
+  }
 
   override def getSijoitteluAjo(sijoitteluajoId: Long): Option[SijoitteluAjo] =
     sijoitteluRepository.getSijoitteluajo(sijoitteluajoId).map(_.entity(sijoitteluRepository.getSijoitteluajonHakukohdeOidit(sijoitteluajoId)))
@@ -56,4 +78,56 @@ class ValintarekisteriRaportointiServiceImpl(sijoitteluService: SijoitteluServic
   override def hakemukset(sijoitteluAjo: SijoitteluAjo, hakukohdeOid: HakukohdeOid): List[KevytHakijaDTO] = ???
 
   override def latestSijoitteluAjoForHakukohde(hakuOid: HakuOid, hakukohdeOid: HakukohdeOid): Option[SijoitteluAjo] = ???
+
+  private def konvertoiHakijat(hyvaksytyt: Boolean, ilmanHyvaksyntaa: Boolean, vastaanottaneet: Boolean, hakukohdeOids: java.util.List[String], count: Integer, index: Integer, valintatulokset: java.util.List[Valintatulos], hakukohteet: java.util.List[Hakukohde]): HakijaPaginationObject = {
+    def filter(hakija: HakijaDTO, hyvaksytyt: Boolean, ilmanHyvaksyntaa: Boolean, vastaanottaneet: Boolean, hakukohdeOids: java.util.List[String]): Boolean = {
+          var isPartOfHakukohdeList = false
+          var isHyvaksytty = false
+          var isVastaanottanut = false
+          import scala.collection.JavaConversions._
+          for (hakutoiveDTO <- hakija.getHakutoiveet) {
+            if (hakukohdeOids != null && hakukohdeOids.contains(hakutoiveDTO.getHakukohdeOid)) isPartOfHakukohdeList = true
+            if (hakutoiveDTO.getVastaanottotieto eq ValintatuloksenTila.VASTAANOTTANUT_SITOVASTI) isVastaanottanut = true
+            import scala.collection.JavaConversions._
+            for (valintatapajono <- hakutoiveDTO.getHakutoiveenValintatapajonot) {
+              if ((valintatapajono.getTila eq HakemuksenTila.HYVAKSYTTY) || (valintatapajono.getTila eq VARASIJALTA_HYVAKSYTTY)) isHyvaksytty = true
+            }
+          }
+          ((hakukohdeOids == null || hakukohdeOids.size <= 0) || isPartOfHakukohdeList) &&
+            ((!hyvaksytyt || isHyvaksytty) && (!ilmanHyvaksyntaa || !isHyvaksytty) && (!vastaanottaneet || isVastaanottanut))
+        }
+
+    def applyPagination(result: java.util.List[HakijaDTO], count: Integer, index: Integer): java.util.List[HakijaDTO] = {
+      if (index != null && count != null) {
+        result.subList(index, Math.min(index + count, result.size - 1))
+      } else if (index != null) {
+        result.subList(index, result.size - 1)
+      } else if (count != null) {
+        result.subList(0, Math.min(count, result.size - 1))
+      } else {
+        result
+      }
+    }
+
+
+    val hakukohdeDTOs = sijoitteluTulosConverter.convert(hakukohteet)
+      val hakijat = raportointiConverter.convert(hakukohdeDTOs, valintatulokset)
+      sort(hakijat, new HakijaDTOComparator)
+      val paginationObject: HakijaPaginationObject = new HakijaPaginationObject
+      val result: java.util.List[HakijaDTO] = new java.util.ArrayList[HakijaDTO]
+      import scala.collection.JavaConversions._
+      for (hakija <- hakijat) {
+        if (filter(hakija, hyvaksytyt, ilmanHyvaksyntaa, vastaanottaneet, hakukohdeOids)) result.add(hakija)
+      }
+      paginationObject.setTotalCount(result.size)
+      paginationObject.setResults(applyPagination(result, count, index))
+      paginationObject
+    }
+}
+
+case class SyntheticSijoitteluAjoForHakusWithoutSijoittelu(hakuOid: HakuOid) extends SijoitteluAjo {
+  setHakuOid(hakuOid.toString)
+  setSijoitteluajoId(-1L)
+  setStartMils(-1L)
+  setEndMils(-1L)
 }
