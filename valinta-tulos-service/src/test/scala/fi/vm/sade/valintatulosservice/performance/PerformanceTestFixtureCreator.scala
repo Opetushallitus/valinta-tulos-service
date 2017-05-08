9575a82c3a739cb25eb60be8fc13d8a2fbd43d57
Jessika Penttinen <jessika.penttinen@siili.com>
2017-05-08 10:32:40 +0300

    Poistettu sijoitteluContext VtsAppConfigista

--- valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/performance/PerformanceTestFixtureCreator.scala
+++ valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/performance/PerformanceTestFixtureCreator.scala
@@ -4,6 +4,7 @@ import fi.vm.sade.utils.slf4j.Logging
 import fi.vm.sade.valintatulosservice.config.VtsAppConfig
 import fi.vm.sade.valintatulosservice.config.VtsAppConfig.VtsAppConfig
 import fi.vm.sade.valintatulosservice.generatedfixtures.{GeneratedFixture, RandomizedGeneratedHakuFixture}
+import fi.vm.sade.valintatulosservice.sijoittelu.legacymongo.SijoitteluSpringContext
 import fi.vm.sade.valintatulosservice.tarjonta.HakuService
 
 object PerformanceTestFixtureCreator extends App with Logging {
@@ -12,8 +13,8 @@ object PerformanceTestFixtureCreator extends App with Logging {
   appConfig.start
 
   private val randomData: RandomizedGeneratedHakuFixture = new RandomizedGeneratedHakuFixture(100, 100000, kohteitaPerHakemus = 5)
-
-  new GeneratedFixture(randomData).apply(appConfig)
+  lazy val sijoitteluContext = new SijoitteluSpringContext(appConfig, SijoitteluSpringContext.createApplicationContext(appConfig))
+  new GeneratedFixture(randomData).apply(sijoitteluContext)(appConfig)
 
   logger.info("fixture applied")
 }
