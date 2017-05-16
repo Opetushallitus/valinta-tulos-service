2c6801636ee9c5e0fc3cb6c6aebc9bb36489224f
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2017-05-16 13:22:44 +0300

    Move HakijanVastaanottoActionSerializer to where it's used
    Expose it only in HakijanVastaanottoServlet which needs it.

--- valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/HakijanVastaanottoServlet.scala
+++ valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/HakijanVastaanottoServlet.scala
@@ -2,9 +2,13 @@ package fi.vm.sade.valintatulosservice
 
 import fi.vm.sade.valintatulosservice.config.VtsAppConfig.VtsAppConfig
 import fi.vm.sade.valintatulosservice.valintarekisteri.domain.{HakemusOid, HakijanVastaanotto, HakijanVastaanottoAction, HakukohdeOid}
+import org.json4s.JsonAST.{JField, JString, JValue}
 import org.json4s._
+import org.json4s.jackson.compactJson
 import org.scalatra.swagger.SwaggerSupportSyntax.OperationBuilder
 import org.scalatra.swagger._
+
+import scala.util.Try
 
 class HakijanVastaanottoServlet(vastaanottoService: VastaanottoService)(implicit val swagger: Swagger, appConfig: VtsAppConfig) extends VtsServletBase {
 
@@ -35,4 +39,23 @@ class HakijanVastaanottoServlet(vastaanottoService: VastaanottoService)(implicit
     vastaanottoService.vastaanotaHakijana(HakijanVastaanotto(personOid, hakemusOid, hakukohdeOid, action))
       .left.foreach(e => throw e)
   }
+
+  private class HakijanVastaanottoActionSerializer extends CustomSerializer[HakijanVastaanottoAction]((formats: Formats) => {
+    def throwMappingException(json: String, cause: Option[Exception] = None) = {
+      val message = s"Can't convert $json to ${classOf[HakijanVastaanottoAction].getSimpleName}. Expected one of ${HakijanVastaanottoAction.values.toSet}"
+      cause match {
+        case Some(e) => throw new MappingException(s"$message : ${e.getMessage}", e)
+        case None => throw new MappingException(message)
+      }
+    }
+    ( {
+      case json@JObject(JField("action", JString(action)) :: Nil) => Try(HakijanVastaanottoAction(action)).recoverWith {
+        case cause: Exception => throwMappingException(compactJson(json), Some(cause))
+      }.get
+      case json: JValue => throwMappingException(compactJson(json))
+    }, {
+      case x: HakijanVastaanottoAction => JObject(JField("action", JString(x.toString)))
+    })
+  })
+
 }
--- valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/customSerializers.scala
+++ valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/customSerializers.scala
@@ -1,34 +1,13 @@
 package fi.vm.sade.valintatulosservice
 
-import java.time.{OffsetDateTime, ZoneId}
 import java.time.format.DateTimeFormatter
+import java.time.{OffsetDateTime, ZoneId}
 
 import fi.vm.sade.sijoittelu.domain.{HakemuksenTila, ValintatuloksenTila}
 import fi.vm.sade.sijoittelu.tulos.dto.IlmoittautumisTila
 import fi.vm.sade.valintatulosservice.valintarekisteri.domain._
-import org.json4s.JsonAST.{JField, JString, JValue}
-import org.json4s.jackson.compactJson
-import org.json4s.{CustomSerializer, Formats, JObject, MappingException}
-
-import scala.util.Try
-
-class HakijanVastaanottoActionSerializer extends CustomSerializer[HakijanVastaanottoAction]((formats: Formats) => {
-  def throwMappingException(json: String, cause: Option[Exception] = None) = {
-    val message = s"Can't convert $json to ${classOf[HakijanVastaanottoAction].getSimpleName}. Expected one of ${HakijanVastaanottoAction.values.toSet}"
-    cause match {
-      case Some(e) => throw new MappingException(s"$message : ${e.getMessage}", e)
-      case None => throw new MappingException(message)
-    }
-  }
-  ( {
-    case json@JObject(JField("action", JString(action)) :: Nil) => Try(HakijanVastaanottoAction(action)).recoverWith {
-      case cause: Exception => throwMappingException(compactJson(json), Some(cause))
-    }.get
-    case json: JValue => throwMappingException(compactJson(json))
-  }, {
-    case x: HakijanVastaanottoAction => JObject(JField("action", JString(x.toString)))
-  })
-})
+import org.json4s.JsonAST.{JString, JValue}
+import org.json4s.{CustomSerializer, Formats}
 
 class VirkailijanVastaanottoActionSerializer extends CustomSerializer[VirkailijanVastaanottoAction]((formats: Formats) => {
   ( {
