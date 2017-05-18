ec451d855b7d333d65706a6b371f45c7a8e33e40
Jessika Penttinen <jessika.penttinen@siili.com>
2017-05-18 15:13:19 +0300

    KevytHakijaDto:n haku vain hakukohteen tietojen kanssa

--- valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/sijoittelu/ValintarekisteriRaportointiServiceSpec.scala
+++ valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/sijoittelu/ValintarekisteriRaportointiServiceSpec.scala
@@ -111,5 +111,56 @@ class ValintarekisteriRaportointiServiceSpec extends ITSpecification with Valint
     }
   }
 
+  "hakemukset vain hakukohteen tietojen kanssa" should {
+    "return hakijat and only one hakutoive for hakukohde (hakukohde without sijoittelu)" in {
+      val kevytHakijaDtot1 = raportointiService.hakemuksetVainHakukohteenTietojenKanssa(sijoitteluajo, oidHaku2hakukohde1)
+      kevytHakijaDtot1.size must_== 5
+      List(hakemusOid5.toString, hakemusOid6.toString, hakemusOid7.toString, hakemusOid8.toString, sijoittelunHakemusOid2.toString).diff(
+        kevytHakijaDtot1.map(_.getHakemusOid)) must_== List()
+
+      assertErillishakuHakija(kevytHakijaDtot1.find(_.getHakemusOid.equals(hakemusOid5.toString)).get, oidHaku2hakukohde1, oidHaku2hakukohde1jono1)
+      assertErillishakuHakija(kevytHakijaDtot1.find(_.getHakemusOid.equals(hakemusOid6.toString)).get, oidHaku2hakukohde1, oidHaku2hakukohde1jono1)
+      assertErillishakuHakija(kevytHakijaDtot1.find(_.getHakemusOid.equals(hakemusOid7.toString)).get, oidHaku2hakukohde1, oidHaku2hakukohde1jono2)
+      assertErillishakuHakija(kevytHakijaDtot1.find(_.getHakemusOid.equals(hakemusOid8.toString)).get, oidHaku2hakukohde1, oidHaku2hakukohde1jono2)
+
+      kevytHakijaDtot1.find(_.getHakemusOid.equals(sijoittelunHakemusOid2.toString)).foreach { hakija =>
+        hakija.getHakutoiveet.size must_== 1
+        val hakutoive = hakija.getHakutoiveet.asScala.find(_.getHakukohdeOid.equals(oidHaku2hakukohde1.toString)).get
+        hakutoive.getHakutoiveenValintatapajonot.size must_== 2
+        val valintatapajono1 = hakutoive.getHakutoiveenValintatapajonot.asScala.find(_.getValintatapajonoOid.equals(oidHaku2hakukohde1jono1.toString)).get
+        valintatapajono1.getTila must_== fi.vm.sade.sijoittelu.tulos.dto.HakemuksenTila.HYVAKSYTTY
+        valintatapajono1.getIlmoittautumisTila must_== fi.vm.sade.sijoittelu.tulos.dto.IlmoittautumisTila.LASNA
+        valintatapajono1.getJonosija must_== null
+        val valintatapajono2 = hakutoive.getHakutoiveenValintatapajonot.asScala.find(_.getValintatapajonoOid.equals(oidHaku2hakukohde1jono2.toString)).get
+        valintatapajono2.getTila must_== fi.vm.sade.sijoittelu.tulos.dto.HakemuksenTila.HYLATTY
+        valintatapajono2.getIlmoittautumisTila must_== fi.vm.sade.sijoittelu.tulos.dto.IlmoittautumisTila.LASNA
+        valintatapajono2.getJonosija must_== null
+      }
+      true must_== true
+    }
+
+    "return hakijat and only one hakutoive for hakukohde (hakukohde with sijoittelu)" in {
+      val kevytHakijaDtot = raportointiService.hakemuksetVainHakukohteenTietojenKanssa(sijoitteluajo, sijoittelunHakukohdeOid2)
+
+      kevytHakijaDtot.size must_== 8
+      List("Haku2.2.1.1", "Haku2.2.1.2", "Haku2.2.2.1", "Haku2.2.2.2", "Haku2.2.3.1", "Haku2.2.3.2", "Haku2.2.4.1", "Haku2.2.4.2").diff(
+        kevytHakijaDtot.map(_.getHakemusOid)
+      ) must_== List()
+
+      kevytHakijaDtot.foreach{ hakija =>
+        hakija.getHakutoiveet.size must_== 1
+        val hakutoive1 = hakija.getHakutoiveet.asScala.find(_.getHakukohdeOid.equals(sijoittelunHakukohdeOid2.toString)).get
+        hakutoive1.getHakutoiveenValintatapajonot.size must_== 1
+        hakutoive1.isKaikkiJonotSijoiteltu must_== true
+        val valintatapajono1 = hakutoive1.getHakutoiveenValintatapajonot.asScala.head
+        hakija.getHakemusOid.startsWith(valintatapajono1.getValintatapajonoOid) must_== true
+        valintatapajono1.getTila must_== fi.vm.sade.sijoittelu.tulos.dto.HakemuksenTila.HYLATTY
+        valintatapajono1.getIlmoittautumisTila must_== fi.vm.sade.sijoittelu.tulos.dto.IlmoittautumisTila.EI_TEHTY
+        valintatapajono1.getJonosija must_== Integer.parseInt("" + hakija.getHakijaOid.charAt(hakija.getHakijaOid.length - 1))
+      }
+      true must_== true
+    }
+  }
+
   step(deleteAll())
 }
