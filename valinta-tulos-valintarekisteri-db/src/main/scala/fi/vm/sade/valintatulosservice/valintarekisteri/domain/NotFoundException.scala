f423a0cbb0fb25ae87eeb212942561caafc6b145
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2017-05-19 12:27:27 +0300

    HK-6 : Fix comment

--- valinta-tulos-valintarekisteri-db/src/main/scala/fi/vm/sade/valintatulosservice/valintarekisteri/domain/NotFoundException.scala
+++ valinta-tulos-valintarekisteri-db/src/main/scala/fi/vm/sade/valintatulosservice/valintarekisteri/domain/NotFoundException.scala
@@ -1,8 +1,11 @@
 package fi.vm.sade.valintatulosservice.valintarekisteri.domain
 
 /**
-  * Throw this exception to return HTTP 404.
+  * You can throw this exception to signal that HTTP 404 should be returned,
+  * but you need to add handling it to each endpoint you want to return 404 from.
+  * The general error handling in <code>VtsServletBase</code> treats all 404
+  * situations as bad paths.
   *
-  * @see fi.vm.sade.valintatulosservice.VtsServletBase#error
+  * @see fi.vm.sade.valintatulosservice.VtsServletBase#notFound
   */
 class NotFoundException(message: String) extends RuntimeException(message)
