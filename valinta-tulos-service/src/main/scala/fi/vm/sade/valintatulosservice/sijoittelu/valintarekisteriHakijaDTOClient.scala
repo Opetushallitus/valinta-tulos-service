bedcbe159de4a83a5c5553fabc485f272c6ebd19
Jessika Penttinen <jessika.penttinen@siili.com>
2017-05-24 13:32:04 +0300

    Implementoitu valintarekisteriHakijaDTOClient

--- valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/sijoittelu/valintarekisteriHakijaDTOClient.scala
+++ valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/sijoittelu/valintarekisteriHakijaDTOClient.scala
@@ -1,12 +1,28 @@
 package fi.vm.sade.valintatulosservice.sijoittelu
 
 import fi.vm.sade.sijoittelu.tulos.dto.raportointi.HakijaDTO
-import fi.vm.sade.valintatulosservice.valintarekisteri.domain.HakuOid
+import fi.vm.sade.valintatulosservice.valintarekisteri.db.{SijoitteluRepository, ValinnantulosRepository}
+import fi.vm.sade.valintatulosservice.valintarekisteri.domain.{HakuOid, SyntheticSijoitteluAjoForHakusWithoutSijoittelu}
+import scala.collection.JavaConverters._
 
 trait ValintarekisteriHakijaDTOClient {
   def processSijoittelunTulokset[T](hakuOid: HakuOid, sijoitteluajoId: String, processor: HakijaDTO => T)
 }
 
-class ValintarekisteriHakijaDTOClientImpl extends ValintarekisteriHakijaDTOClient {
-  override def processSijoittelunTulokset[T](hakuOid: HakuOid, sijoitteluajoId: String, processor: (HakijaDTO) => T): Unit = ???
+class ValintarekisteriHakijaDTOClientImpl(raportointiService: ValintarekisteriRaportointiService,
+                                          sijoittelunTulosClient: ValintarekisteriSijoittelunTulosClient,
+                                          repository: SijoitteluRepository with ValinnantulosRepository) extends ValintarekisteriHakijaDTOClient {
+
+  override def processSijoittelunTulokset[T](hakuOid: HakuOid, sijoitteluajoId: String, processor: (HakijaDTO) => T) = {
+
+    (sijoitteluajoId match {
+      case x if repository.isLatest(x) => sijoittelunTulosClient.fetchLatestSijoitteluAjo(hakuOid)
+      case x => repository.parseId(x).flatMap {
+        case id if 0 < id => raportointiService.getSijoitteluAjo(id)
+        case _ => Some(SyntheticSijoitteluAjoForHakusWithoutSijoittelu(hakuOid))
+      }
+    }) foreach { sijoitteluajo =>
+      raportointiService.hakemukset(sijoitteluajo).getResults.asScala.foreach(processor)
+    }
+  }
 }
