c9a7fba84b9930c9015f954c3e531177bf0e6c2c
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2017-05-31 18:36:52 +0300

    tests fixed

--- valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/local/KelaKoulutusSpec.scala
+++ valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/local/KelaKoulutusSpec.scala
@@ -12,41 +12,41 @@ class KelaKoulutusSpec extends Specification {
 
     "map 'Alempi'" in {
 
-      KelaKoulutus(Seq(KoulutusLaajuusarvo(Some("623701"), Some("180")))) must_== Some(KelaKoulutus(Some("050"),"180",None))
+      KelaKoulutus(Seq(KoulutusLaajuusarvo(None, Some("623701"), Some("180")))) must_== Some(KelaKoulutus(Some("050"),"180",None))
 
     }
 
     "map 'Ylempi'" in {
 
-        KelaKoulutus(Seq(KoulutusLaajuusarvo(Some("799999"), Some("120")))) must_== Some(KelaKoulutus(Some("061"),"120",None))
+        KelaKoulutus(Seq(KoulutusLaajuusarvo(None, Some("799999"), Some("120")))) must_== Some(KelaKoulutus(Some("061"),"120",None))
 
     }
 
     "map 'Ylempi' and 'Alempi'" in {
 
-      KelaKoulutus(Seq(KoulutusLaajuusarvo(Some("799999"), Some("180")),
-        KoulutusLaajuusarvo(Some("623701"), Some("120")))) must_== Some(KelaKoulutus(Some("060"),"120",Some("180")))
+      KelaKoulutus(Seq(KoulutusLaajuusarvo(None, Some("799999"), Some("180")),
+        KoulutusLaajuusarvo(None, Some("623701"), Some("120")))) must_== Some(KelaKoulutus(Some("060"),"120",Some("180")))
 
     }
 
     "map 'Lääkäri'" in {
 
-      KelaKoulutus(Seq(KoulutusLaajuusarvo(Some("772101"), Some("180")))) must_== Some(KelaKoulutus(Some("070"),"180",None))
-      KelaKoulutus(Seq(KoulutusLaajuusarvo(Some("772201"), Some("120")))) must_== Some(KelaKoulutus(Some("071"),"120",None))
+      KelaKoulutus(Seq(KoulutusLaajuusarvo(None, Some("772101"), Some("180")))) must_== Some(KelaKoulutus(Some("070"),"180",None))
+      KelaKoulutus(Seq(KoulutusLaajuusarvo(None, Some("772201"), Some("120")))) must_== Some(KelaKoulutus(Some("071"),"120",None))
 
     }
 
     "map 'Ylempi' and 'Alempi' with hidden sisältyväkoulutuskoodi" in {
 
       KelaKoulutus(Seq(
-        KoulutusLaajuusarvo(Some("623701"), Some("180+120")), KoulutusLaajuusarvo(Some("726701"), None),
-        KoulutusLaajuusarvo(Some("799999"), Some("120")))) must_== Some(KelaKoulutus(Some("060"),"180",Some("120")))
+        KoulutusLaajuusarvo(None, Some("623701"), Some("180+120")), KoulutusLaajuusarvo(None, Some("726701"), None),
+        KoulutusLaajuusarvo(None, Some("799999"), Some("120")))) must_== Some(KelaKoulutus(Some("060"),"180",Some("120")))
 
     }
 
     "map 'Ylempi' and 'Alempi' with hidden sisältyväkoulutuskoodi" in {
 
-      KelaKoulutus(Seq(KoulutusLaajuusarvo(Some("751301"), Some("120")))) must_== Some(KelaKoulutus(Some("061"),"120", None))
+      KelaKoulutus(Seq(KoulutusLaajuusarvo(None, Some("751301"), Some("120")))) must_== Some(KelaKoulutus(Some("061"),"120", None))
 
     }
   }
