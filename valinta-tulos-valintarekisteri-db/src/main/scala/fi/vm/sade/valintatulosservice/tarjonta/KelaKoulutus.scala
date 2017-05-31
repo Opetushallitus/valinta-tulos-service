694245cd91e806bf763279004d384aa42f85c32e
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2017-05-31 18:36:52 +0300

    tests fixed

--- valinta-tulos-valintarekisteri-db/src/main/scala/fi/vm/sade/valintatulosservice/tarjonta/KelaKoulutus.scala
+++ valinta-tulos-valintarekisteri-db/src/main/scala/fi/vm/sade/valintatulosservice/tarjonta/KelaKoulutus.scala
@@ -180,4 +180,4 @@ object KelaKoulutus {
   }
 
 }
-case class KoulutusLaajuusarvo(oid: String, koulutuskoodi: Option[String], opintojenLaajuusarvo: Option[String])
+case class KoulutusLaajuusarvo(oid: Option[String], koulutuskoodi: Option[String], opintojenLaajuusarvo: Option[String])
