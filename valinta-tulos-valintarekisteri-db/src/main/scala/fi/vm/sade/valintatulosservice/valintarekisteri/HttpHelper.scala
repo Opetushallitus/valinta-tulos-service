cf52c1845186300070b8abcbb4f7792cd9e37a99
Heikki Honkanen <heikki.honkanen@gofore.com>
2017-10-12 13:05:38 +0300

    WIP

---
+++ valinta-tulos-valintarekisteri-db/src/main/scala/fi/vm/sade/valintatulosservice/valintarekisteri/HttpHelper.scala
@@ -0,0 +1,41 @@
+package fi.vm.sade.valintatulosservice.valintarekisteri
+
+import fi.vm.sade.utils.http.DefaultHttpClient
+import org.json4s.jackson.JsonMethods.parse
+
+import scala.util.Try
+import scala.util.control.NonFatal
+import scalaj.http.HttpOptions
+
+trait HttpHelper {
+  private def parseStatus(json: String): Option[String] = {
+    for {
+      status <- (parse(json) \ "status").extractOpt[String]
+    } yield status
+  }
+
+  def fetch[T](url: String)(parse: (String => T)): Either[Throwable, T] = {
+    Try(DefaultHttpClient.httpGet(
+      url,
+      HttpOptions.connTimeout(30000),
+      HttpOptions.readTimeout(120000)
+    ).header("clientSubSystemCode", "valinta-tulos-service")
+      .header("Caller-id", "valinta-tulos-service")
+      .responseWithHeaders match {
+      case (200, _, resultString) if parseStatus(resultString).contains("NOT_FOUND") =>
+        Left(new IllegalArgumentException(s"GET $url failed with status 200: NOT_FOUND"))
+      case (404, _, resultString) =>
+        Left(new IllegalArgumentException(s"GET $url failed with status 404: $resultString"))
+      case (200, _, resultString) =>
+        Try(Right(parse(resultString))).recover {
+          case NonFatal(e) => Left(new IllegalStateException(s"Parsing result $resultString of GET $url failed", e))
+        }.get
+      case (502, _, _) =>
+        Left(new RuntimeException(s"GET $url failed with status 502"))
+      case (responseCode, _, resultString) =>
+        Left(new RuntimeException(s"GET $url failed with status $responseCode: $resultString"))
+    }).recover {
+      case NonFatal(e) => Left(new RuntimeException(s"GET $url failed", e))
+    }.get
+  }
+}
