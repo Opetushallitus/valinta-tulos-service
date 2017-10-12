06aca10539305f163959a47c96c83971f4f775cb
Heikki Honkanen <heikki.honkanen@gofore.com>
2017-10-12 14:38:31 +0300

    Fix compile.

--- valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/hakemus/ataruHakemusRepository.scala
+++ valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/hakemus/ataruHakemusRepository.scala
@@ -1,7 +1,7 @@
 package fi.vm.sade.valintatulosservice.hakemus
 
-import fi.vm.sade.properties.OphProperties
 import fi.vm.sade.valintatulosservice.HttpHelper
+import fi.vm.sade.valintatulosservice.config.VtsAppConfig.VtsAppConfig
 import fi.vm.sade.valintatulosservice.domain.Henkilotiedot
 import fi.vm.sade.valintatulosservice.json.JsonFormats
 import fi.vm.sade.valintatulosservice.valintarekisteri.domain._
@@ -14,11 +14,11 @@ case class AtaruHakemus(oid: HakemusOid,
                         hakukohteet: List[String],
                         henkilotiedot: Henkilotiedot)
 
-class AtaruHakemusRepository(config: OphProperties) extends JsonFormats{
-  def getHakemukset(hakuOid: HakuOid): Either[Throwable, Haku] = {
-    val url = config.url("ataru-service.applications", hakuOid)
+class AtaruHakemusRepository(config: VtsAppConfig) extends JsonFormats{
+  def getHakemukset(hakuOid: HakuOid): Either[Throwable, List[AtaruHakemus]] = {
+    val url = config.ophUrlProperties.url("ataru-service.applications", hakuOid)
     HttpHelper.fetch(url) { response =>
-      val hakemukset: List[AtaruHakemus] = (parse(response) \ "result").extract[List[AtaruHakemus]]
+      parse(response).extract[List[AtaruHakemus]]
     }.left.map {
       case e: IllegalArgumentException => new IllegalArgumentException(s"No applications for $hakuOid found", e)
       case e: IllegalStateException => new IllegalStateException(s"Parsing applications for $hakuOid failed", e)
