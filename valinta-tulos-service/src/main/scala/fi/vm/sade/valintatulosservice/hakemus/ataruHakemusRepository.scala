619be4ae944f96d0da786c8da6f38cadf0c7ede9
Jani Rahkola <jani.rahkola@reaktor.com>
2017-10-16 10:09:09 +0300

    Refactor creation of params

--- valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/hakemus/ataruHakemusRepository.scala
+++ valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/hakemus/ataruHakemusRepository.scala
@@ -6,6 +6,7 @@ import fi.vm.sade.valintatulosservice.domain.Henkilotiedot
 import fi.vm.sade.valintatulosservice.json.JsonFormats
 import fi.vm.sade.valintatulosservice.valintarekisteri.domain._
 import org.json4s.jackson.JsonMethods._
+
 import scala.collection.JavaConverters._
 
 case class AtaruHakemus(oid: HakemusOid,
@@ -19,10 +20,12 @@ class AtaruHakemusRepository(config: VtsAppConfig) extends JsonFormats {
   def getHakemukset(hakuOid: Option[HakuOid], hakukohdeOid: Option[HakukohdeOid],
                     hakemusOids: Option[List[HakemusOid]]): Either[Throwable, List[AtaruHakemus]] = {
     if (hakuOid.isEmpty && hakemusOids.isEmpty) throw new IllegalArgumentException("Must specify either hakuOid or hakemusOid(s)")
-    val params = ((if (hakuOid.isDefined) Seq("hakuOid" -> hakuOid.get) else Nil) ++
-      (if (hakukohdeOid.isDefined) Seq("hakukohdeOid" -> hakukohdeOid.get) else Nil) ++
-      (if (hakemusOids.isDefined) Seq("hakemusOids" -> hakemusOids.get) else Nil)).toMap.asJava
-    val url = config.ophUrlProperties.url("ataru-service.applications", params)
+    val params = (
+      hakuOid.map("hakuOid" -> _.toString) ++
+        hakukohdeOid.map("hakukohdeOid" -> _.toString) ++
+        hakemusOids.map("hakemusOids" -> _.map(_.toString))
+      ).toMap
+    val url = config.ophUrlProperties.url("ataru-service.applications", params.asJava)
     HttpHelper.fetch(url) { response =>
       parse(response).extract[List[AtaruHakemus]]
     }.left.map {
