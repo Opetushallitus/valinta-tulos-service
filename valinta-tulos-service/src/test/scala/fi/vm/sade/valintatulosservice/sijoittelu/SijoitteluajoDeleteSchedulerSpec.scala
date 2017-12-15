f483bc66e06be48eeaa6b9a1aa731ec5252047ae
Mikko Siukola <mikko.siukola@cybercom.com>
2017-12-15 16:21:07 +0200

    Korjattu testi

--- valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/sijoittelu/SijoitteluajoDeleteSchedulerSpec.scala
+++ valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/sijoittelu/SijoitteluajoDeleteSchedulerSpec.scala
@@ -17,16 +17,20 @@ class SijoitteluajoDeleteSchedulerSpec extends Specification with MockitoMatcher
     val repository = mock[DeleteSijoitteluRepository]
     val scheduler = new SijoitteluajoDeleteScheduler(repository, 23, 3)
 
+    repository.acquireLockForSijoitteluajoCleaning(55) returns Seq(true)
+    repository.clearLockForSijoitteluajoCleaning(55) returns Seq(true)
     repository.listHakuAndSijoitteluAjoCount() returns Seq((HakuOid("1"),3), (HakuOid("2"),5), (HakuOid("3"),6))
     repository.findSijoitteluAjotSkippingFirst(HakuOid("2"),3) returns Seq(1l,2l)
     repository.findSijoitteluAjotSkippingFirst(HakuOid("3"),3) returns Seq(3l,4l,5l)
 
     def checkConditions() = {
+      there was one (repository).acquireLockForSijoitteluajoCleaning(55)
       there was one (repository).listHakuAndSijoitteluAjoCount()
       there was two (repository).findSijoitteluAjotSkippingFirst(any[HakuOid],anyInt)
       there was two (repository).deleteSijoitteluajot(any[HakuOid],any[Seq[Long]])
       there was one (repository).deleteSijoitteluajot(HakuOid("2"),Seq(1l,2l))
       there was one (repository).deleteSijoitteluajot(HakuOid("3"),Seq(3l,4l,5l))
+      there was one (repository).clearLockForSijoitteluajoCleaning(55)
     }
   }
 
