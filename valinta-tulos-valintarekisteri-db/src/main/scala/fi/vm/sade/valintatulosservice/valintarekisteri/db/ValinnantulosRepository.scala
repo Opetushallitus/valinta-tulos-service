b69966d535b81866b0f5ffb3fe08a4d97a860078
Tatu Honkanen <tatu.honkanen@cybercom.com>
2018-06-18 16:33:02 +0300

    Jonokäsittely lisätty.

--- valinta-tulos-valintarekisteri-db/src/main/scala/fi/vm/sade/valintatulosservice/valintarekisteri/db/ValinnantulosRepository.scala
+++ valinta-tulos-valintarekisteri-db/src/main/scala/fi/vm/sade/valintatulosservice/valintarekisteri/db/ValinnantulosRepository.scala
@@ -28,7 +28,7 @@ trait ValinnantulosRepository extends ValintarekisteriRepository {
 
   def getMuutoshistoriaForHakemus(hakemusOid: HakemusOid, valintatapajonoOid: ValintatapajonoOid): List[Muutos]
 
-  def getViimeisinValinnantilaMuutosHistoriasta(hakemusOid: HakemusOid, hakukohdeOid: HakukohdeOid): Option[Valinnantila]
+  def getViimeisinValinnantilaMuutosHistoriasta(hakemusOid: HakemusOid, hakukohdeOid: HakukohdeOid): Int
 
   def getValinnantuloksetForHakukohde(hakukohdeOid: HakukohdeOid): DBIO[Set[Valinnantulos]]
   def getValinnantuloksetForValintatapajono(valintatapajonoOid: ValintatapajonoOid): Set[Valinnantulos]
