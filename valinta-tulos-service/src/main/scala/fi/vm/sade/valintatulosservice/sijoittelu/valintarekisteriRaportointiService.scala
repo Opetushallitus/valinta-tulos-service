32634421c2bf82020a6ba6a74ba8062a78bed1b1
teo.mertanen <teo.mertanen@gofore.com>
2018-07-03 12:56:33 +0300

    BUG-1809 remove RaportointiConverterImpl & optimize imports

--- valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/sijoittelu/valintarekisteriRaportointiService.scala
+++ valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/sijoittelu/valintarekisteriRaportointiService.scala
@@ -1,22 +1,19 @@
 package fi.vm.sade.valintatulosservice.sijoittelu
 
-import java.util
 import java.util.Collections.sort
 
-import fi.vm.sade.sijoittelu.domain.{Hakukohde, SijoitteluAjo, Valintatulos}
+import fi.vm.sade.sijoittelu.domain.SijoitteluAjo
 import fi.vm.sade.sijoittelu.tulos.dto.HakemuksenTila.VARASIJALTA_HYVAKSYTTY
 import fi.vm.sade.sijoittelu.tulos.dto.raportointi.{HakijaDTO, HakijaPaginationObject, KevytHakijaDTO}
 import fi.vm.sade.sijoittelu.tulos.dto.{HakemuksenTila, ValintatuloksenTila}
 import fi.vm.sade.sijoittelu.tulos.service.impl.comparators.HakijaDTOComparator
-import fi.vm.sade.sijoittelu.tulos.service.impl.converters.{RaportointiConverterImpl, SijoitteluTulosConverterImpl}
 import fi.vm.sade.utils.Timer.timed
 import fi.vm.sade.utils.slf4j.Logging
 import fi.vm.sade.valintatulosservice.valintarekisteri.db.{HakijaRepository, SijoitteluRepository, ValinnantulosRepository}
-import fi.vm.sade.valintatulosservice.valintarekisteri.domain.{HakemusRecord, HakuOid, HakukohdeOid, _}
-import fi.vm.sade.valintatulosservice.valintarekisteri.sijoittelu.{SijoitteluajonHakijat, SijoitteluajonHakukohteet}
+import fi.vm.sade.valintatulosservice.valintarekisteri.domain.{HakuOid, HakukohdeOid, _}
+import fi.vm.sade.valintatulosservice.valintarekisteri.sijoittelu.SijoitteluajonHakijat
 
 import scala.collection.JavaConverters._
-import fi.vm.sade.valintatulosservice.memoize.TTLOptionalMemoize
 import scala.util.{Failure, Success, Try}
 
 trait ValintarekisteriRaportointiService {
