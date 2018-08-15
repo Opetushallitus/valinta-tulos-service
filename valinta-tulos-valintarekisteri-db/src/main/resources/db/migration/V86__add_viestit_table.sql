9d094449cbe1977cd5109de20126f3c97c4c632b
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2018-08-15 15:18:04 +0300

    BUG-1834 : Optimise migration a bit
    Defer running it to manual afterwork after
    the installation.

--- valinta-tulos-valintarekisteri-db/src/main/resources/db/migration/V86__add_viestit_table.sql
+++ valinta-tulos-valintarekisteri-db/src/main/resources/db/migration/V86__add_viestit_table.sql
@@ -7,7 +7,7 @@ values ('VASTAANOTTOILMOITUS'),
        ('EHDOLLISEN_PERIYTYMISEN_ILMOITUS'),
        ('SITOVAN_VASTAANOTON_ILMOITUS');
 
-create table viestit (
+create table viestit_preliminary (
     hakemus_oid text not null,
     hakukohde_oid text not null,
     syy text references viestin_syy,
@@ -18,8 +18,35 @@ create table viestit (
     primary key (hakemus_oid, hakukohde_oid)
 );
 
-insert into viestit(hakemus_oid, hakukohde_oid, syy, lahetetty, lahettaminen_aloitettu)
-(select vo.hakemus_oid,
+/*
+
+-- NB: Running the old data migration requires too much time to do it
+-- automatically as a part of the Flyway migration run on service startup.
+-- Hence, this must be run separately.
+-- It _must_ be completed before the first valinta-tulos-emailer run takes place!
+
+insert into viestit_preliminary(hakemus_oid, hakukohde_oid, syy, lahetetty, lahettaminen_aloitettu)
+(with nv as (
+    select all_vastaanotot.henkilo,
+           all_vastaanotot.hakukohde,
+           all_vastaanotot.action,
+           all_vastaanotot.ilmoittaja
+    from (select vastaanotot.henkilo,
+                 vastaanotot.hakukohde,
+                 vastaanotot.action,
+                 vastaanotot.ilmoittaja
+          from vastaanotot
+          where (vastaanotot.deleted is null and action in ('VastaanotaSitovasti', 'VastaanotaEhdollisesti'))
+          union
+          select henkiloviitteet.linked_oid as henkilo,
+                 vastaanotot.hakukohde,
+                 vastaanotot.action,
+                 vastaanotot.ilmoittaja
+          from (vastaanotot
+              join henkiloviitteet on vastaanotot.henkilo = henkiloviitteet.person_oid)
+          where (vastaanotot.deleted is null and action in ('VastaanotaSitovasti', 'VastaanotaEhdollisesti'))) all_vastaanotot
+    )
+select vo.hakemus_oid,
         vo.hakukohde_oid,
         case when
             nv.action is not distinct from 'VastaanotaSitovasti' and
@@ -32,16 +59,14 @@ insert into viestit(hakemus_oid, hakukohde_oid, syy, lahetetty, lahettaminen_alo
              else 'VASTAANOTTOILMOITUS'
             end,
         vo.sent,
-        vo.previous_check
- from viestinnan_ohjaus as vo
- join valinnantilat as vt
- on vt.valintatapajono_oid = vo.valintatapajono_oid and
-    vt.hakemus_oid = vo.hakemus_oid and
-    vt.hakukohde_oid = vo.hakukohde_oid
- left join newest_vastaanotot as nv
- on nv.henkilo = vt.henkilo_oid and
-    nv.hakukohde = vt.hakukohde_oid
- where vo.sent is not null);
+        max(vo.previous_check)
+from viestinnan_ohjaus vo
+join valinnantilat vt on vt.hakemus_oid = vo.hakemus_oid and vt.valintatapajono_oid = vo.valintatapajono_oid and vt.hakukohde_oid = vo.hakukohde_oid
+left join nv on nv.henkilo = vt.henkilo_oid and nv.hakukohde = vo.hakukohde_oid and nv.hakukohde = vt.hakukohde_oid
+where vo.sent is not null
+group by vo.hakemus_oid, vo.hakukohde_oid, nv.action, nv.ilmoittaja, vo.sent);
+
+alter table viestit_preliminary rename to viestit;
 
 create table viestit_history (like viestit);
 
@@ -82,3 +107,5 @@ create trigger delete_viestit_history
     after delete on viestit
     for each row
 execute procedure update_viestit_history();
+
+*/
