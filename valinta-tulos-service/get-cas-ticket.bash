13cd72e5645a48b9d78b4edd97e80e080d0b67e9
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2018-10-31 09:26:54 +0200

    Just print service ticket by default
    This makes it easier to use this in scripts.

--- valinta-tulos-service/get-cas-ticket.bash
+++ valinta-tulos-service/get-cas-ticket.bash
@@ -6,7 +6,7 @@ if [[ ! -x `which http` ]]; then
 fi
 
 if [ -z $2 ]; then
-  echo "Usage: $0 <username> <password> [virkailija base url] [target service URL]"
+  echo "Usage: $0 <username> <password> [virkailija base url] [target service URL] [--verbose]"
   exit 3
 fi
 
@@ -25,14 +25,27 @@ else
   SERVICE=$4
 fi
 
-echo "Making the TGT request with:"
+if [ "$5" == "--verbose" ]; then
+  VERBOSE=true
+else
+  VERBOSE=false
+fi
+
+function print_if_verbose() {
+  if [ $VERBOSE == "true" ]; then
+    echo "$1"
+  fi
+}
+
+print_if_verbose "Making the TGT request with:"
 TGT_REQUEST="http --pretty none --form --print h POST $VIRKAILIJA/cas/v1/tickets username=$USERNAME password=$PASSWORD"
-echo $TGT_REQUEST
+
+print_if_verbose $TGT_REQUEST
 TGT_LOCATION_LINE=`$TGT_REQUEST | grep Location | cut -f 2 -d ' '`
-echo "TGT_LOCATION_LINE: $TGT_LOCATION_LINE"
+print_if_verbose "TGT_LOCATION_LINE: $TGT_LOCATION_LINE"
 TGT_LOCATION=`echo ${TGT_LOCATION_LINE} | awk '{gsub(/[[:cntrl:]]/,"")}1'`
 
-echo "Making the service ticket request to \"$TGT_LOCATION\" "
+print_if_verbose "Making the service ticket request to \"$TGT_LOCATION\" "
 
-http --form --print b POST $TGT_LOCATION service=$SERVICE
+http --form --print b POST $TGT_LOCATION service=$SERVICE | egrep '^ST'
 
