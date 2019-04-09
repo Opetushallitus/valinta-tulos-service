5838acdf5b106434e5d66db446b4482e2fb911ac
teo.mertanen <teo.mertanen@gofore.com>
2019-04-09 14:03:31 +0300

    OY-250 Swagger in EmailerServlet

--- valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosemailer/EmailerServlet.scala
+++ valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosemailer/EmailerServlet.scala
@@ -1,14 +1,28 @@
 package fi.vm.sade.valintatulosemailer
 
 import fi.vm.sade.utils.slf4j.Logging
-import org.scalatra.{Ok, ScalatraServlet}
+import fi.vm.sade.valintatulosservice.{VtsServletBase, VtsSwaggerBase}
+import org.scalatra.Ok
+import org.scalatra.swagger.SwaggerSupportSyntax.OperationBuilder
+import org.scalatra.swagger.{SwaggerSupport, _}
 
 
-class EmailerServlet(emailerService: EmailerService) extends ScalatraServlet with Logging {
+class EmailerServlet(emailerService: EmailerService)(implicit val swagger: Swagger) extends VtsServletBase with EmailerSwagger with Logging {
 
-  post("/run") {
+  post("/run", operation(postRunEmailerSwagger)) {
     println("EmailerServlet POST called")
     emailerService.run()
     Ok()
   }
+}
+
+trait EmailerSwagger extends VtsSwaggerBase { this: SwaggerSupport =>
+  override val applicationName = Some("ensikertalaisuus")
+  override protected def applicationDescription: String = "Ensikertalaisuus-tiedon vastaanotto-osuus"
+
+  val postRunEmailerSwagger: OperationBuilder = apiOperation("postRunEmailer")
+    .summary("Aja sähköpostien lähetys")
+    .notes("Vastaanottosähköpostien manuaalinen lähetys. Tulisi ajaa myös automaattisesti 15 minuutin välein.")
+    .responseMessage(ModelResponseMessage(400, "Kuvaus virheellisestä pyynnöstä"))
+    .responseMessage(ModelResponseMessage(500, "Virhe palvelussa"))
 }
