7141b3a5da1d04d92d7e9edc626ce7e08f24dab6
teo.mertanen <teo.mertanen@gofore.com>
2019-04-11 14:17:48 +0300

    OY-250 cron-style scheduling in emailer
    OY-250 logging changes

--- valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosemailer/EmailerService.scala
+++ valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosemailer/EmailerService.scala
@@ -1,13 +1,15 @@
 package fi.vm.sade.valintatulosemailer
 
-import com.github.kagkarlsson.scheduler.task.schedule.FixedDelay
 import fi.vm.sade.utils.Timer
 import fi.vm.sade.utils.slf4j.Logging
 import fi.vm.sade.valintatulosemailer.config.EmailerConfigParser
 import fi.vm.sade.valintatulosemailer.config.EmailerRegistry.EmailerRegistry
 import fi.vm.sade.valintatulosservice.valintarekisteri.db.impl.ValintarekisteriDb
+
+import scala.collection.JavaConverters.seqAsJavaListConverter
 //import scopt.OptionParser
 
+import com.github.kagkarlsson.scheduler.task.schedule.CronSchedule
 import com.github.kagkarlsson.scheduler.Scheduler
 import com.github.kagkarlsson.scheduler.task._
 import com.github.kagkarlsson.scheduler.task.helper.Tasks
@@ -28,8 +30,11 @@ import scala.util.Try
 //}
 
 class EmailerService(registry: EmailerRegistry, db: ValintarekisteriDb) extends Logging {
+  logger.info("***** VT-emailer initializing *****")
+  logger.info(s"Using settings: " +
+    s"${registry.settings.withOverride("ryhmasahkoposti.cas.password", "***" )(EmailerConfigParser())}")
 
-  private val voidExecutionHandler: VoidExecutionHandler[Void] = new VoidExecutionHandler[Void] {
+  private val executionHandler: VoidExecutionHandler[Void] = new VoidExecutionHandler[Void] {
     logger.info("Scheduled task execution handler setup")
 
     override def execute(taskInstance: TaskInstance[Void], executionContext: ExecutionContext): Unit = {
@@ -37,19 +42,24 @@ class EmailerService(registry: EmailerRegistry, db: ValintarekisteriDb) extends 
       run()
     }
   }
-  private val delay: FixedDelay = FixedDelay.ofMinutes(15)
-  private val quarterlyTask = Tasks.recurring("quarterly-emailer-task", delay).execute(voidExecutionHandler)
+
+  private val cronString = "0 0,15,30,45 * ? * *;0 0 7,19 * * ?"
+  private val cronStrings: List[String] = cronString.split(";").toList
+  private val cronTasks = cronStrings.zipWithIndex.map{ case (s,i) =>
+    Tasks.recurring(s"cron-emailer-task-$i", new CronSchedule(s))
+      .execute(executionHandler)
+  }
+
+  logger.info("Scheduled emailer task for cron expressions:\n" + cronStrings.mkString("\n"))
 
   private val numberOfThreads = 5
-  private val scheduler: Scheduler = Scheduler.create(db.dataSource).startTasks(quarterlyTask).threads(numberOfThreads).build
+  private val scheduler: Scheduler = Scheduler.create(db.dataSource).startTasks(cronTasks.asJava).threads(numberOfThreads).build
 
   // hourlyTask is automatically scheduled on startup if not already started (i.e. exists in the db)
   scheduler.start()
 
   def run(): Unit = {
-    logger.info("***** VT-emailer started *****")
-    logger.info(s"Using settings: " +
-      s"${registry.settings.withOverride("ryhmasahkoposti.cas.password", "***" )(EmailerConfigParser())}")
+    logger.info("***** VT-emailer run starting *****")
     Try(Timer.timed("Batch send") {
       val ids = registry.mailer.sendMail
       if (ids.nonEmpty) {
