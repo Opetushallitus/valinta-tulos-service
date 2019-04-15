9020bfaa7f42d1051e7701c8e7402d93c2236a59
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2019-04-15 14:02:02 +0300

    BUG-1976 : Add test

--- valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/streamingresults/HakemustenTulosHakuLock.scala
+++ valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/streamingresults/HakemustenTulosHakuLock.scala
@@ -1,26 +1,27 @@
 package fi.vm.sade.valintatulosservice.streamingresults
 
+import java.util.concurrent.TimeUnit.SECONDS
 import java.util.concurrent.locks.ReentrantLock
 import java.util.concurrent.{Semaphore, TimeUnit}
 
 import fi.vm.sade.utils.slf4j.Logging
 
-class HakemustenTulosHakuLock(queueLimit: Int, lockDurationSeconds: Int) extends Logging {
+class HakemustenTulosHakuLock(queueLimit: Int, lockDuration: Int, lockDurationTimeUnit: TimeUnit = SECONDS) extends Logging {
   private val lockQueue: Semaphore = new Semaphore(queueLimit + 1)
   private val loadingLock: ReentrantLock = new ReentrantLock(true)
 
   def execute[T](operation: () => T): Either[String, T] = {
     if (lockQueue.tryAcquire()) {
       try {
-        if (loadingLock.tryLock(lockDurationSeconds, TimeUnit.SECONDS)) {
+        if (loadingLock.tryLock(lockDuration, lockDurationTimeUnit)) {
           try {
             Right(operation())
           } finally {
             loadingLock.unlock()
           }
         } else {
-          Left(s"Acquiring lock timed out after $lockDurationSeconds" +
-            s" seconds: No available capacity for this request, please try again later")
+          Left(s"Acquiring lock timed out after $lockDuration" +
+            s" ${lockDurationTimeUnit.toString.toLowerCase}: No available capacity for this request, please try again later")
         }
       } finally {
         lockQueue.release()
---
+++ valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/streamingresults/HakemustenTulosHakuLockSpec.scala
@@ -0,0 +1,56 @@
+package fi.vm.sade.valintatulosservice.streamingresults
+
+import java.util.concurrent.TimeUnit.SECONDS
+import java.util.concurrent.{Executors, TimeUnit}
+
+import org.junit.runner.RunWith
+import org.specs2.mutable.Specification
+import org.specs2.runner.JUnitRunner
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, ExecutionContextExecutor, Future}
+
+@RunWith(classOf[JUnitRunner])
+class HakemustenTulosHakuLockSpec extends Specification {
+  private implicit val ec: ExecutionContextExecutor = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(10))
+
+  private val operationLagMillis: Int = 500
+  private val fastLockMillis: Int = operationLagMillis / 10
+  private val slowLockMillis: Int = (operationLagMillis * 1.5).toInt
+
+  private val fastLock: HakemustenTulosHakuLock = new HakemustenTulosHakuLock(queueLimit = 2, lockDuration = fastLockMillis, TimeUnit.MILLISECONDS)
+  private val slowLock: HakemustenTulosHakuLock = new HakemustenTulosHakuLock(queueLimit = 2, lockDuration = slowLockMillis, TimeUnit.MILLISECONDS)
+
+  "HakemustenTulosHakuLock" should {
+    "control concurrent access" in {
+      val fast1 = runSlowFuture(fastLock)
+      val fast2 = runSlowFuture(fastLock)
+      val fast3 = runSlowFuture(fastLock)
+      val fast4 = runSlowFuture(fastLock)
+
+      val slow1 = runSlowFuture(slowLock)
+      val slow2 = runSlowFuture(slowLock)
+      val slow3 = runSlowFuture(slowLock)
+
+
+      Await.result(fast1, Duration(1, SECONDS)) must beRight("ok")
+      Await.result(fast2, Duration(1, SECONDS)) must beLeft(s"Acquiring lock timed out after $fastLockMillis milliseconds: No available capacity for this request, please try again later")
+      Await.result(fast3, Duration(1, SECONDS)) must beLeft(s"Acquiring lock timed out after $fastLockMillis milliseconds: No available capacity for this request, please try again later")
+      Await.result(fast4, Duration(1, SECONDS)) must beLeft(s"Results loading queue of size 2 full: No available capacity for this request, please try again later")
+
+      Await.result(slow1, Duration(1, SECONDS)) must beRight("ok")
+      Await.result(slow2, Duration(1, SECONDS)) must beRight("ok")
+      Await.result(slow3, Duration(1, SECONDS)) must beLeft(s"Acquiring lock timed out after $slowLockMillis milliseconds: No available capacity for this request, please try again later")
+    }
+  }
+
+  private def runSlowFuture(lock: HakemustenTulosHakuLock): Future[Either[String, String]] = {
+    Thread.sleep(2)
+    Future {
+      lock.execute[String](() => {
+        Thread.sleep(operationLagMillis)
+        "ok"
+      })
+    }
+  }
+}
