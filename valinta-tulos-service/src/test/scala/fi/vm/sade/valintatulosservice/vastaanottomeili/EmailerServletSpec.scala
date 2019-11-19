575718352460d78625a3c2557f9c7f249ed4916d
teo.mertanen <teo.mertanen@gofore.com>
2019-11-19 13:49:06 +0200

    BUG-2086 add testSession to EmailerServletSpec

--- valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/vastaanottomeili/EmailerServletSpec.scala
+++ valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/vastaanottomeili/EmailerServletSpec.scala
@@ -1,18 +1,23 @@
 package fi.vm.sade.valintatulosservice.vastaanottomeili
 
 import fi.vm.sade.valintatulosservice.ServletSpecification
+import fi.vm.sade.valintatulosservice.json.JsonFormats
+import fi.vm.sade.valintatulosservice.valintarekisteri.ValintarekisteriDbTools
 import org.junit.runner.RunWith
 import org.specs2.runner.JUnitRunner
 
 
 @RunWith(classOf[JUnitRunner])
-class EmailerServletSpec extends ServletSpecification {
-  private val httpHeaders: Map[String, String] = Map("Content-type" -> "application/json")
+class EmailerServletSpec extends ServletSpecification with ValintarekisteriDbTools {
+  override implicit val formats = JsonFormats.jsonFormats
+  lazy val testSession = createTestSession()
+
+  private lazy val httpHeadersWithSession: Map[String, String] = Map("Cookie" -> s"session=${testSession}", "Content-type" -> "application/json")
 
   "POST /emailer/run" should {
     "be OK" in {
       val uri = "auth/emailer/run"
-      post(uri, headers = httpHeaders) {
+      post(uri, headers = httpHeadersWithSession) {
         status must_== 200
       }
     }
