7e4af8095c89ecee9b6d3c0584582c05fc5e9889
Jaakko Heikelä <jaakko.heikela@gofore.com>
2020-06-02 15:18:39 +0300

    OY-374: Remove unnecessary explicit index creation.

--- valinta-tulos-valintarekisteri-db/src/main/resources/db/migration-vts/V99__paremmat_indeksit_valinnantulokset_tauluun.sql
+++ valinta-tulos-valintarekisteri-db/src/main/resources/db/migration-vts/V99__paremmat_indeksit_valinnantulokset_tauluun.sql
@@ -6,5 +6,3 @@ alter table valinnantulokset add constraint valinnantulokset_pkey_hakemus_oid_en
     primary key (hakemus_oid, valintatapajono_oid, hakukohde_oid);
 alter table ehdollisen_hyvaksynnan_ehto add constraint ehdollisen_hyvaksynnan_ehto_valinnantulokset_hakemus_oid_ensin
     foreign key (hakemus_oid, valintatapajono_oid, hakukohde_oid) references valinnantulokset (hakemus_oid, valintatapajono_oid, hakukohde_oid);
-create unique index valinnantulokset_pkey_idx
-    on valinnantulokset (hakemus_oid, valintatapajono_oid, hakukohde_oid);
