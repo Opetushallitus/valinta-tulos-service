a7ad761cf47ae63f22a7b392137a78439297f6c2
August Kilponen <august.kilponen@gmail.com>
2022-05-03 20:56:16 +0300

    Muutettu migraatioita sellaisiksi että ne toimivat myös PostgreSQL 12 kanssa

--- valinta-tulos-valintarekisteri-db/src/main/resources/db/migration-vts/V49__change_valinnantulokset_pkey_order.sql
+++ valinta-tulos-valintarekisteri-db/src/main/resources/db/migration-vts/V49__change_valinnantulokset_pkey_order.sql
@@ -1,4 +1,5 @@
-alter table viestinnan_ohjaus drop constraint viestinnan_ohjaus_hakukohde_oid_fkey;
+alter table viestinnan_ohjaus drop constraint if exists viestinnan_ohjaus_hakukohde_oid_fkey;
+alter table viestinnan_ohjaus drop constraint if exists viestinnan_ohjaus_hakukohde_oid_valintatapajono_oid_hakemu_fkey;
 alter table valinnantulokset drop constraint valinnantulokset_pkey;
 alter table valinnantulokset add primary key (valintatapajono_oid, hakemus_oid, hakukohde_oid);
 drop index valinnantulokset_history_by_valinnantulokset_pkey;
