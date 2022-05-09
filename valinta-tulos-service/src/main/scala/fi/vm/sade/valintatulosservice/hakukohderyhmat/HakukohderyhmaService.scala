0b17b7cbcf27c147420552f88a1bb508b2f4dc61
Tatu Honkanen <tatu.honkanen@cybercom.com>
2022-05-09 16:51:27 +0300

    OY-3358 : Java version to 11, Fix HakukohderyhmaService

--- valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/hakukohderyhmat/HakukohderyhmaService.scala
+++ valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/hakukohderyhmat/HakukohderyhmaService.scala
@@ -1,61 +1,94 @@
 package fi.vm.sade.valintatulosservice.hakukohderyhmat
 
-import fi.vm.sade.utils.cas.{CasAuthenticatingClient, CasClient, CasParams}
+import fi.vm.sade.javautils.nio.cas.{CasClient, CasClientBuilder, CasConfig}
 import fi.vm.sade.utils.slf4j.Logging
 import fi.vm.sade.valintatulosservice.config.VtsAppConfig.VtsAppConfig
 import fi.vm.sade.valintatulosservice.valintarekisteri.domain.{HakukohdeOid, HakukohderyhmaOid}
-import org.http4s.client.blaze.SimpleHttp1Client
-import org.http4s.json4s.native.jsonExtract
-import org.http4s.{Method, Request, Uri}
-import org.json4s.{DefaultFormats, Formats}
-import scalaz.concurrent.Task
+import org.asynchttpclient.{RequestBuilder, Response}
+import org.http4s.Method
+import org.json4s.jackson.JsonMethods._
 
-import java.util.concurrent.TimeUnit
-import scala.concurrent.duration.Duration
+import java.util.concurrent.CompletableFuture
+import scala.compat.java8.FutureConverters.toScala
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+object ScalaCasConfig {
+
+  def apply(username: String,
+            password: String,
+            casUrl: String,
+            serviceUrl: String,
+            csrf: String,
+            callerId: String,
+            serviceUrlSuffix: String,
+            jSessionName: String): CasConfig = {
+
+    new CasConfig.CasConfigBuilder(
+      username,
+      password,
+      casUrl,
+      serviceUrl,
+      csrf,
+      callerId,
+      serviceUrlSuffix
+    ).setJsessionName(jSessionName).build()
+  }
+}
+
 
 class HakukohderyhmaService(appConfig: VtsAppConfig,
                             casClient: CasClient) extends Logging {
-  private implicit val jsonFormats: Formats = DefaultFormats
-  private val casParams = CasParams(
-    appConfig.ophUrlProperties.url("hakukohderyhmapalvelu.suffix"),
-    "auth/cas",
-    appConfig.settings.securitySettings.casUsername,
-    appConfig.settings.securitySettings.casPassword
-  )
+  protected val loginParams: String = "/auth/cas"
+  protected val sessionCookieName: String = "ring-session"
+  protected val serviceName: String = appConfig.ophUrlProperties.url("hakukohderyhmapalvelu.service")
+  protected val callerId: String = appConfig.settings.callerId
+  protected val casUrl: String = appConfig.ophUrlProperties.url("cas.url")
 
-  private val client = CasAuthenticatingClient(
-    casClient = casClient,
-    casParams = casParams,
-    serviceClient = SimpleHttp1Client(appConfig.blazeDefaultConfig),
-    clientCallerId = appConfig.settings.callerId,
-    sessionCookieName = "ring-session"
-  )
-
-  def getHakukohderyhmat(oid: HakukohdeOid): Either[Throwable, Seq[HakukohderyhmaOid]] = {
-    fetch[Seq[HakukohderyhmaOid]](appConfig.ophUrlProperties.url(appConfig.ophUrlProperties.url("hakukohderyhmapalvelu.hakukohderyhmat", oid)))
+  lazy protected val client: CasClient = {
+    CasClientBuilder.build(ScalaCasConfig(
+      appConfig.settings.securitySettings.casUsername,
+      appConfig.settings.securitySettings.casPassword,
+      casUrl,
+      serviceName,
+      callerId,
+      callerId,
+      loginParams,
+      sessionCookieName)
+    )
   }
 
-  def getHakukohteet(oid: HakukohderyhmaOid): Either[Throwable, Seq[HakukohdeOid]] = {
-    fetch[Seq[HakukohdeOid]](appConfig.ophUrlProperties.url(appConfig.ophUrlProperties.url("hakukohderyhmapalvelu.hakukohteet", oid)))
+  def getHakukohderyhmat(oid: HakukohdeOid): Future[Seq[HakukohderyhmaOid]] = {
+    fetch(Method.GET, appConfig.ophUrlProperties.url("hakukohderyhmapalvelu.hakukohderyhmat", oid)).flatMap {
+      case response if response.getStatusCode.equals(200) => Future.successful(parse(response.getResponseBody).values.asInstanceOf[Seq[String]].map(s => HakukohderyhmaOid(s)))
+      case failure =>
+        val errorString = s"Hakukohderyhmät fetch failed for hakukohdeoid: $oid with status ${failure.getStatusCode}, body: ${failure.getResponseBody}"
+            logger.error(errorString)
+            Future.failed(
+              new RuntimeException(errorString)
+            )
+
+    }
   }
 
-  private def fetch[T](url: String)(implicit manifest: Manifest[T]): Either[Throwable, T] = {
-    Uri.fromString(url).flatMap(uri => client.fetch(Request(method = Method.GET, uri = uri)) {
-      case r if r.status.code == 200 =>
-        r.as[T](jsonExtract[T])
-          .handleWith {
-            case t => r.bodyAsText.runLog
-              .map(_.mkString)
-              .flatMap(body => Task.fail(new IllegalStateException(s"Parsing result $body of GET $url failed", t)))
-          }
-      case r if r.status.code == 404 =>
-        r.bodyAsText.runLog
-          .map(_.mkString)
-          .flatMap(body => Task.fail(new IllegalArgumentException(s"GET $url failed with status 404: $body")))
-      case r =>
-        r.bodyAsText.runLog
-          .map(_.mkString)
-          .flatMap(body => Task.fail(new RuntimeException(s"GET $url failed with status ${r.status.code}: $body")))
-    }.unsafePerformSyncAttemptFor(Duration(1, TimeUnit.MINUTES))).toEither
+  def getHakukohteet(oid: HakukohderyhmaOid): Future[Seq[HakukohdeOid]] = {
+    fetch(Method.GET, appConfig.ophUrlProperties.url("hakukohderyhmapalvelu.hakukohteet", oid)).flatMap {
+      case response if response.getStatusCode.equals(200) => Future.successful(parse(response.getResponseBody).values.asInstanceOf[Seq[String]].map(s => HakukohdeOid(s)))
+      case failure =>
+        val errorString = s"Hakukohteet fetch failed for hakukohderyhmäoid: $oid with status ${failure.getStatusCode}, body: ${failure.getResponseBody}"
+        logger.error(errorString)
+        Future.failed(
+          new RuntimeException(errorString)
+        )
+    }
+  }
+
+  protected def fetch(method: Method, url: String): Future[Response] = {
+    val requestBuilder = new RequestBuilder()
+      .setMethod(method.name)
+      .setUrl(url)
+    val request = requestBuilder.build()
+    val future: CompletableFuture[Response] = client.execute(request)
+    toScala(future)
   }
 }
