1e3371cbd282f16d26d4a58552c0eb1fc116c428
Jussi Jartamo <jussi.jartamo@gofore.com>
2022-07-05 14:24:06 +0300

    openapi swagger

--- valinta-tulos-service/src/main/scala/fi/vm/sade/openapi/OpenAPIServlet.scala
+++ valinta-tulos-service/src/main/scala/fi/vm/sade/openapi/OpenAPIServlet.scala
@@ -9,11 +9,16 @@ import org.scalatra.Ok
 import org.scalatra.swagger.Swagger
 import io.swagger.parser.OpenAPIParser
 import io.swagger.parser.models.ParseOptions
+import org.scalatra.swagger.SwaggerSupportSyntax.OperationBuilder
 
 class OpenAPIServlet(config: VtsAppConfig)(implicit val swagger: Swagger) extends VtsServletBase with VtsSwaggerBase with Logging {
   override protected def applicationDescription: String = "Valinta-tulos-service Open API v3"
 
-  get("/open-api.json") {
+  lazy val getOpenApi: OperationBuilder = (apiOperation[Unit]("open-api.json")
+    summary "Valinta-tulos-service Open API v3"
+    tags "openapi")
+
+  get("/open-api.json", operation(getOpenApi)) {
     val openAPIParser = new OpenAPIParser
     val options = new ParseOptions
     options.setResolveFully(true)
