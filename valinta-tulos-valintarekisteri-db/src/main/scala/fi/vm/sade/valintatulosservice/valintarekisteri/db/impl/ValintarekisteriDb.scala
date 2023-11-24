777a927f6eed54665edc2777bb2a951a780c1f1c
Risto Salama <risto.salama@gofore.com>
2023-11-24 03:20:10 +0200

    Update flyway version to match one in valintalaskenta

--- valinta-tulos-valintarekisteri-db/src/main/scala/fi/vm/sade/valintatulosservice/valintarekisteri/db/impl/ValintarekisteriDb.scala
+++ valinta-tulos-valintarekisteri-db/src/main/scala/fi/vm/sade/valintatulosservice/valintarekisteri/db/impl/ValintarekisteriDb.scala
@@ -5,6 +5,8 @@ import fi.vm.sade.utils.Timer
 import fi.vm.sade.valintatulosservice.valintarekisteri.db.ehdollisestihyvaksyttavissa.HyvaksynnanEhtoRepositoryImpl
 import org.apache.commons.lang3.builder.ToStringBuilder
 import org.flywaydb.core.Flyway
+import org.flywaydb.core.api.Location
+import org.flywaydb.core.api.configuration.FluentConfiguration
 import slick.jdbc.PostgresProfile.api._
 
 case class DbConfig(url: String,
@@ -36,11 +38,16 @@ class ValintarekisteriDb(config: DbConfig, isItProfile:Boolean = false) extends 
   with HyvaksynnanEhtoRepositoryImpl {
 
   logger.info(s"Database configuration: ${config.copy(password = Some("***"))}")
-  val flyway = new Flyway()
-  flyway.setDataSource(config.url, config.user.orNull, config.password.orNull)
-  flyway.setOutOfOrder(true)
-  flyway.setLocations("db/migration-vts")
-  Timer.timed("Flyway migration") { flyway.migrate() }
+  val m: FluentConfiguration = Flyway.configure
+    .dataSource(
+      config.url,
+      config.user.orNull,
+      config.password.orNull
+    )
+    .outOfOrder(true)
+    .locations("db/migration-vts")
+
+  Timer.timed("Flyway migration") { m.load().migrate().migrationsExecuted }
 
   val hikariConfig: HikariConfig = {
     val c = new HikariConfig()
