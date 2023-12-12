6eb8deefcf7da64c8b565e7c667d1ef4db367b5e
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2023-12-12 11:00:22 +0200

    show headers in create-vts-session.sh

--- scripts/create-vts-session.sh
+++ scripts/create-vts-session.sh
@@ -12,4 +12,4 @@ test -z "$TGT_URL" && echo "Use scripts/create-cas-tgt.sh first" && exit 1
 ST=$(curl -s "$TGT_URL" -H 'Caller-id: panutest' --data-urlencode "service=$SERVICE")
 test -z "$ST" && echo "Service ticket was not created" && exit 1
 
-curl -c scripts/cookies.txt -H "ticket: $ST" -H 'caller-id: panutesti' "$LOGINURL"
+curl -i -c scripts/cookies.txt -H "ticket: $ST" -H 'caller-id: panutesti' "$LOGINURL"
