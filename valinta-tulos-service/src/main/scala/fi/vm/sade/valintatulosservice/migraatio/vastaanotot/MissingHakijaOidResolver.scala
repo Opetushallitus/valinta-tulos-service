3e7633a68cd5e988985e27477b0eb2f2211684d1
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2024-01-22 12:29:57 +0200

    Remove unused findPersonOidByHakemusOid and findPersonOidByHetu

--- valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/migraatio/vastaanotot/MissingHakijaOidResolver.scala
+++ valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/migraatio/vastaanotot/MissingHakijaOidResolver.scala
@@ -46,13 +46,10 @@ object HakijaResolver {
 }
 
 class MissingHakijaOidResolver(appConfig: VtsAppConfig) extends JsonFormats with Logging with HakijaResolver {
-  private val hakuClient = createCasClient(appConfig, appConfig.ophUrlProperties.url("haku-app.baseUrl"))
   private val oppijanumerorekisteriClient = createCasClient(appConfig, appConfig.ophUrlProperties.url("url-oppijanumerorekisteri"))
 
   case class HakemusHenkilo(personOid: Option[String], hetu: Option[String], etunimet: String, sukunimi: String, kutsumanimet: String,
                             syntymaaika: String, aidinkieli: Option[String], sukupuoli: Option[String])
-
-  private def findPersonOidByHetu(hetu: String): Option[String] = findPersonByHetu(hetu).map(_.oidHenkilo)
 
   override def findPersonByHetu(hetu: String, timeout: Duration = 60 seconds): Option[Henkilo] = {
     val requestUri = appConfig.ophUrlProperties.url("oppijanumerorekisteri-service.henkiloPerusByHetu", hetu)
@@ -76,46 +73,6 @@ class MissingHakijaOidResolver(appConfig: VtsAppConfig) extends JsonFormats with
       case e: Throwable => {
         handleFailure(e, "searching person oid by hetu " + Option(hetu).map(_.replaceAll(".","*")))
         throw e
-      }
-    }
-  }
-
-  def extractHakemusHenkiloFromResponse(r: JValue): HakemusHenkilo = {
-    val henkilotiedot = r \ "answers" \ "henkilotiedot"
-    HakemusHenkilo( (r \ "personOid").extractOpt[String],
-      (henkilotiedot \ "Henkilotunnus").extractOpt[String],
-      (henkilotiedot \ "Etunimet").extract[String],
-      (henkilotiedot \ "Sukunimi").extract[String],
-      (henkilotiedot \ "Kutsumanimi").extract[String],
-      (henkilotiedot \ "syntymaaika").extract[String],
-      (henkilotiedot \ "aidinkieli").extractOpt[String],
-      (henkilotiedot \ "sukupuoli").extractOpt[String])
-  }
-
-  def findPersonOidByHakemusOid(hakemusOid: String): Option[String] = {
-
-    val requestUri = appConfig.ophUrlProperties.url("haku-app.application.queryBase", hakemusOid)
-    val req = new RequestBuilder()
-      .setUrl(requestUri)
-      .addHeader("Accept", "application/json")
-      .build()
-    //val backoff = RetryPolicy.exponentialBackoff(Duration(30, TimeUnit.SECONDS), maxRetry = 10)
-    //val retryingClient = Retry(RetryPolicy(backoff))(hakuClient)
-    val result = toScala(hakuClient.execute(req)).map {
-      case r if 200 == r.getStatusCode =>
-        extractHakemusHenkiloFromResponse(parse(r.getResponseBodyAsStream)) match {
-          case henkilo if henkilo.personOid.isDefined => henkilo.personOid
-          case henkilo => henkilo.hetu.map(findPersonOidByHetu).getOrElse({throw new RuntimeException(s"Hakemuksella $hakemusOid ei hakijaoidia!")})
-        }
-      case r =>
-        throw new RuntimeException(s"Got non-OK response from haku-app when fetching hakemus $hakemusOid: ${r.toString}")
-    }
-    try {
-      Await.result(result, Duration(3, TimeUnit.MINUTES))
-    } catch {
-      case e: Throwable => {
-        handleFailure(e, s"finding henkilö from hakemus $hakemusOid")
-        throw new RuntimeException(s"Hakemuksen $hakemusOid henkilötietoja ei saatu jäsennetyksi.")
       }
     }
   }
