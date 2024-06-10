c2647a02f23cbb3555a95911afbdb8687fa7d729
Mikko Siukola <mikko.siukola@cybercom.com>
2024-06-10 13:22:48 +0300

    OK-565 Logging improvements

--- ovara-valinta-tulos/src/main/scala/fi/vm/sade/valintatulosservice/ovara/ajastus/SiirtotiedostoApp.scala
+++ ovara-valinta-tulos/src/main/scala/fi/vm/sade/valintatulosservice/ovara/ajastus/SiirtotiedostoApp.scala
@@ -9,12 +9,12 @@ import fi.vm.sade.valintatulosservice.ovara
 import fi.vm.sade.valintatulosservice.ovara.{SiirtotiedostoPalveluClient, SiirtotiedostoService}
 
 object SiirtotiedostoApp {
-  private val logger: Logger = LoggerFactory.getLogger(SiirtotiedostoApp.getClass)
+  private val logger: Logger = LoggerFactory.getLogger("fi.vm.sade.valintatulosservice.ovara.ajastus.SiirtotiedostoApp")
 
   def main(args: Array[String]): Unit = {
     logger.info(s"Hello, ovara world!")
-
-    implicit val appConfig: VtsAppConfig = VtsAppConfig.fromString("ovara")
+    println(s"Hello, ovara world!")
+    val appConfig: VtsAppConfig = VtsAppConfig.fromString("ovara")
     appConfig.start
     val siirtotiedostoConfig = appConfig.settings.siirtotiedostoConfig
 
