0170558cb60c5d4e82a155c97f014292c09cc2c9
Mikko Siukola <mikko.siukola@cybercom.com>
2024-06-11 16:53:58 +0300

    OK-565 WIP Improve timed siirtotiedosto process and success/error handling

--- valinta-tulos-valintarekisteri-db/src/main/resources/db/migration-vts/V20242205000000__create_siirtotiedostot_table.sql
+++ valinta-tulos-valintarekisteri-db/src/main/resources/db/migration-vts/V20242205000000__create_siirtotiedostot_table.sql
@@ -1,15 +1,23 @@
-create table siirtotiedosto (
-                                 id serial, --todo, voisi katsoa, jos kaikkeen tiedostojen muodostamiseen liittyvään lokitukseen tai ainakin virheisiin saataisiin mukaan tämä id
-                                 window_start varchar not null,
-                                 window_end varchar not null,
-                                 run_start timestamp with time zone,
-                                 run_end timestamp with time zone,
-                                 success boolean,
-                                 error_message varchar, -- Tyhjä string, jos mikään ei mennyt vikaan
-                                 PRIMARY KEY (id)
+create table if not exists siirtotiedostot (
+                                             id serial, --todo, voisi katsoa, jos kaikkeen tiedostojen muodostamiseen liittyvään lokitukseen tai ainakin virheisiin saataisiin mukaan tämä id
+                                             uuid varchar,
+                                             window_start varchar not null,
+                                             window_end varchar not null,
+                                             run_start timestamp with time zone,
+                                             run_end timestamp with time zone,
+                                             info jsonb, --ainakin tilastot tiedostoihin päätyneistä entiteettimääristä tyypeittäin, {"counts": {"tyyppi1": 300, "tyyppi2": 13}}
+                                             success boolean,
+                                             error_message varchar, -- Tyhjä string, jos mikään ei mennyt vikaan
+                                             PRIMARY KEY (id)
 );
+
+COMMENT ON column siirtotiedostot.run_start IS 'Siirtotiedosto-operaation suorituksen alkuaika';
+COMMENT ON column siirtotiedostot.run_end IS 'Siirtotiedosto-operaation suorituksen loppuaika';
+COMMENT ON column siirtotiedostot.info IS 'Tietoja tallennetuista entiteeteistä, mm. lukumäärät';
+COMMENT ON column siirtotiedostot.error_message IS 'null, jos mikään ei mennyt vikaan';
 
 --alter table siirtotiedosto owner to oph;
 
 --todo, what are the initial values? This is just something to get the poc started.
-insert into siirtotiedosto(id, window_start, window_end, run_start, run_end, success, error_message) values (1, '1990-01-01 10:41:20.538107 +00:00', '2024-04-10 10:41:20.538107 +00:00', now(), now(), true, null) on conflict do nothing;
+insert into siirtotiedostot(id, uuid, window_start, window_end, run_start, run_end, info, success, error_message)
+    values (1, '57be2612-ba79-429e-a93e-c38346f1d62d',  '1990-01-01 10:41:20.538107 +00:00', '2024-04-10 10:41:20.538107 +00:00', now(), now(), '{"entityTotals": {}}'::jsonb, true, null) on conflict do nothing;
