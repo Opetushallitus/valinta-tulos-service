6d4e3750b306b1ca16681314a43f6a8826500ea8
Topias Heinonen <topias.heinonen@gofore.com>
2025-10-08 13:38:19 +0300

    OY-5242 Kopioidaan java-http:n ainoa käytetty metodi tähän repoon

--- valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/casAuthenticatedServlet.scala
+++ valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/casAuthenticatedServlet.scala
@@ -1,14 +1,15 @@
 package fi.vm.sade.valintatulosservice
 
-import java.net.InetAddress
-import java.util.UUID
-
-import fi.vm.sade.javautils.http.HttpServletRequestUtils
+import com.typesafe.scalalogging.LazyLogging
 import fi.vm.sade.security.{AuthenticationFailedException, AuthorizationFailedException}
 import fi.vm.sade.valintatulosservice.logging.Logging
 import fi.vm.sade.valintatulosservice.security.{Role, Session}
 import fi.vm.sade.valintatulosservice.valintarekisteri.db.SessionRepository
 import org.scalatra._
+
+import java.net.InetAddress
+import java.util.UUID
+import javax.servlet.http.HttpServletRequest
 
 trait CasAuthenticatedServlet { this:ScalatraServlet with Logging =>
   def sessionRepository: SessionRepository
@@ -30,6 +31,27 @@ trait CasAuthenticatedServlet { this:ScalatraServlet with Logging =>
       Authenticated.unapply(authenticated).get, InetAddress.getByName(HttpServletRequestUtils.getRemoteAddress(request)),
         request.headers.get("User-Agent").getOrElse(throw new IllegalArgumentException("Otsake User-Agent on pakollinen."))
     )
+  }
+}
+
+object HttpServletRequestUtils extends LazyLogging {
+  def getRemoteAddress(httpServletRequest: HttpServletRequest): String =
+    getRemoteAddress(httpServletRequest.getHeader("X-Real-IP"), httpServletRequest
+      .getHeader("X-Forwarded-For"), httpServletRequest
+      .getRemoteAddr, httpServletRequest.getRequestURI)
+
+  private def getRemoteAddress(xRealIp: String,
+                               xForwardedFor: String,
+                               remoteAddr: String,
+                               requestURI: String): String = {
+    val isNotBlank = (txt: String) => txt != null && txt.nonEmpty
+    if (isNotBlank(xRealIp)) return xRealIp
+    if (isNotBlank(xForwardedFor)) {
+      if (xForwardedFor.contains(",")) logger.error(s"Could not find X-Real-IP header, but X-Forwarded-For contains multiple values: $xForwardedFor, this can cause problems")
+      return xForwardedFor
+    }
+    logger.warn(s"X-Real-IP or X-Forwarded-For was not set. Are we not running behind a load balancer? Request URI is $requestURI")
+    remoteAddr
   }
 }
 
