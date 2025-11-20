3d89e86ba41566613976cc5ade8b4619afc303b9
Topias Heinonen <topias.heinonen@gofore.com>
2025-11-20 15:19:58 +0200

    OY-5163 Uusi käyttäjätietojen välitystapa (#438)
    Poistetaan käyttöoikeus-clientin käyttäminen CAS-tunnistautumisen
    yhteydessä.

--- valinta-tulos-service/src/main/scala/fi/vm/sade/security/MockSecurityContext.scala
+++ valinta-tulos-service/src/main/scala/fi/vm/sade/security/MockSecurityContext.scala
@@ -1,19 +1,22 @@
 package fi.vm.sade.security.mock
 
+import fi.vm.sade.javautils.nio.cas.UserDetails
 import fi.vm.sade.javautils.nio.cas.impl.{CasClientImpl, CasSessionFetcher}
 import fi.vm.sade.security.{ScalaCasConfig, SecurityContext}
-import fi.vm.sade.valintatulosservice.kayttooikeus.KayttooikeusUserDetails
 import fi.vm.sade.valintatulosservice.security.Role
 import org.asynchttpclient.Dsl._
 
 import java.util.concurrent.{CompletableFuture, TimeUnit}
-import scala.concurrent.duration.{Duration, SECONDS}
+import scala.collection.JavaConverters._
+import scala.concurrent.duration.{Duration, FiniteDuration, SECONDS}
 
-class MockSecurityContext(val casServiceIdentifier: String, val requiredRoles: Set[Role], users: Map[String, KayttooikeusUserDetails]) extends SecurityContext {
+class MockSecurityContext(val casServiceIdentifier: String, val requiredRoles: Set[Role]) extends SecurityContext {
 
-  def validateServiceTicketTimeout = Duration(1, SECONDS)
+  val validateServiceTicketTimeout: FiniteDuration = Duration(1, SECONDS)
+
   private val casConfig = ScalaCasConfig("", "", "", "", "", "vts-test-caller-id", "", "session")
   private val httpClient = asyncHttpClient()
+
   val javaCasClient = Some(
     new CasClientImpl(
       casConfig,
@@ -28,10 +31,10 @@ class MockSecurityContext(val casServiceIdentifier: String, val requiredRoles: S
           CompletableFuture.completedFuture("session-token-from-mock-context")
 
       }) {
-      override def validateServiceTicketWithVirkailijaUsername(service: String, ticket: String): CompletableFuture[String] = {
+      override def validateServiceTicketWithVirkailijaUserDetails(service: String, ticket: String): CompletableFuture[UserDetails] = {
         if (ticket.startsWith(MockSecurityContext.ticketPrefix(service))) {
-          val username = ticket.stripPrefix(MockSecurityContext.ticketPrefix(service))
-          CompletableFuture.completedFuture(username)
+          val user = new UserDetails("testuser", "1.2.246.562.24.64735725450", "", "", requiredRoles.map(_.s).asJava)
+          CompletableFuture.completedFuture(user)
         } else {
           CompletableFuture.failedFuture(new RuntimeException("unrecognized ticket: " + ticket))
         }
--- valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/local/CasSessionServiceSpec.scala
+++ valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/local/CasSessionServiceSpec.scala
@@ -1,12 +1,8 @@
 package fi.vm.sade.valintatulosservice.local
 
-import java.util.UUID
-import java.util.concurrent.TimeUnit
-
-import fi.vm.sade.security.{AuthenticationFailedException, CasSessionService, ProductionSecurityContext}
-import fi.vm.sade.javautils.nio.cas.CasClient
+import fi.vm.sade.javautils.nio.cas.{CasClient, UserDetails}
+import fi.vm.sade.security.{AuthenticationFailedException, CasSessionService, ProductionSecurityContext, SecurityContext}
 import fi.vm.sade.valintatulosservice.config.VtsAppConfig.VtsAppConfig
-import fi.vm.sade.valintatulosservice.kayttooikeus.{KayttooikeusUserDetails, KayttooikeusUserDetailsService}
 import fi.vm.sade.valintatulosservice.security.{CasSession, ServiceTicket}
 import fi.vm.sade.valintatulosservice.valintarekisteri.db.SessionRepository
 import org.junit.runner.RunWith
@@ -16,9 +12,10 @@ import org.specs2.mock.mockito.MockitoStubs
 import org.specs2.mutable.Specification
 import org.specs2.runner.JUnitRunner
 import org.specs2.specification.Scope
-import scalaz.concurrent.Task
-import java.util.concurrent.CompletableFuture
 
+import java.util.UUID
+import java.util.concurrent.{CompletableFuture, TimeUnit}
+import scala.collection.JavaConverters._
 import scala.concurrent.duration.Duration
 
 @RunWith(classOf[JUnitRunner])
@@ -34,17 +31,11 @@ class CasSessionServiceSpec extends Specification with MockitoStubs {
     }
     "Authentication fails if session not found and ticket is invalid" in new CasSessionServiceWithMocks {
       sessionRepository.get(id) returns None
-      casClient.validateServiceTicketWithVirkailijaUsername(service, ticket) returns CompletableFuture.failedFuture(new RuntimeException("error"))
+      casClient.validateServiceTicketWithVirkailijaUserDetails(service, ticket) returns CompletableFuture.failedFuture(new RuntimeException("error"))
       cas.getSession(Some(ServiceTicket(ticket)), Some(id)) must beLeft.like { case t => t must beAnInstanceOf[AuthenticationFailedException] }
     }
-    "Authentication fails if session not found, ticket is valid and KO user not found" in new CasSessionServiceWithMocks {
-      sessionRepository.get(id) returns None
-      casClient.validateServiceTicketWithVirkailijaUsername(service, ticket) returns CompletableFuture.completedFuture(uid)
-      userDetailsService.getUserByUsername(uid) returns Left(new IllegalArgumentException("User not found for testing purposes"))
-      cas.getSession(Some(ServiceTicket(ticket)), Some(id)) must beLeft.like { case t => t must beAnInstanceOf[IllegalArgumentException] }
-    }
     "Authentication fails if ticket is invalid" in new CasSessionServiceWithMocks {
-      casClient.validateServiceTicketWithVirkailijaUsername(service, ticket) returns CompletableFuture.failedFuture(new RuntimeException("error for testing"))
+      casClient.validateServiceTicketWithVirkailijaUserDetails(service, ticket) returns CompletableFuture.failedFuture(new RuntimeException("error for testing"))
       cas.getSession(Some(ServiceTicket(ticket)), None) must beLeft.like { case t => t must beAnInstanceOf[AuthenticationFailedException] }
     }
     "Return session if found" in new CasSessionServiceWithMocks {
@@ -53,25 +44,23 @@ class CasSessionServiceSpec extends Specification with MockitoStubs {
     }
     "Return session if found and don't validate ticket" in new CasSessionServiceWithMocks {
       sessionRepository.get(id) returns Some(session)
-      casClient.validateServiceTicketWithVirkailijaUsername(service, ticket) returns CompletableFuture.failedFuture(new RuntimeException("not reached"))
+      casClient.validateServiceTicketWithVirkailijaUserDetails(service, ticket) returns CompletableFuture.failedFuture(new RuntimeException("not reached"))
       cas.getSession(Some(ServiceTicket(ticket)), Some(id)) must beRight((id, session))
     }
     "Return created session" in new CasSessionServiceWithMocks {
-      casClient.validateServiceTicketWithVirkailijaUsername(service, ticket) returns CompletableFuture.completedFuture(uid)
-      userDetailsService.getUserByUsername(uid) returns Right(user)
+      casClient.validateServiceTicketWithVirkailijaUserDetails(service, ticket) returns CompletableFuture.completedFuture(user)
       sessionRepository.store(session) returns newId
       cas.getSession(Some(ServiceTicket(ticket)), None) must beRight((newId, session))
     }
     "Return created session if session not found" in new CasSessionServiceWithMocks {
       sessionRepository.get(id) returns None
-      casClient.validateServiceTicketWithVirkailijaUsername(service, ticket) returns CompletableFuture.completedFuture(uid)
-      userDetailsService.getUserByUsername(uid) returns Right(user)
+      casClient.validateServiceTicketWithVirkailijaUserDetails(service, ticket) returns CompletableFuture.completedFuture(user)
       sessionRepository.store(session) returns newId
       cas.getSession(Some(ServiceTicket(ticket)), Some(id)) must beRight((newId, session))
     }
     "Return exception if fetching session fails and don't validate ticket" in new CasSessionServiceWithMocks {
       sessionRepository.get(id) throws new RuntimeException("error")
-      casClient.validateServiceTicketWithVirkailijaUsername(service, ticket) returns CompletableFuture.failedFuture(new RuntimeException("not reached"))
+      casClient.validateServiceTicketWithVirkailijaUserDetails(service, ticket) returns CompletableFuture.failedFuture(new RuntimeException("not reached"))
       cas.getSession(Some(ServiceTicket(ticket)), Some(id)) must beLeft.like { case t => t must not(beAnInstanceOf[AuthenticationFailedException]) }
     }
   }
@@ -82,12 +71,11 @@ class CasSessionServiceSpec extends Specification with MockitoStubs {
     val ticket: String = "service-ticket"
     val uid: String = "uid"
     val service = "cas-service-identifier"
-    val user = KayttooikeusUserDetails(Set(), "person-oid")
+    val user = new UserDetails(uid, "person-oid", "", "", Set[String]().asJava)
     val session = CasSession(ServiceTicket(ticket), "person-oid", Set())
     val sessionRepository: SessionRepository = mock[SessionRepository]
     val casClient: CasClient = mock[CasClient]
-    val userDetailsService: KayttooikeusUserDetailsService = mock[KayttooikeusUserDetailsService]
-    val securityContext = new ProductionSecurityContext(
+    val securityContext: SecurityContext = new ProductionSecurityContext(
       service, Set(), Duration(1, TimeUnit.SECONDS)
     ) {
       override val javaCasClient = Some(casClient)
@@ -95,7 +83,7 @@ class CasSessionServiceSpec extends Specification with MockitoStubs {
     val appConfig: VtsAppConfig = mock[VtsAppConfig]
 
     val cas: CasSessionService = new CasSessionService(
-      appConfig, securityContext, service, userDetailsService, sessionRepository
+      appConfig, securityContext, service, sessionRepository
     )
   }
 }
