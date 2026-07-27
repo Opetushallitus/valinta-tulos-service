c0c2c5d1ed4900c5448e6776831728b6557697d7
Topias Heinonen <topias.heinonen@gofore.com>
2026-07-27 09:00:55 +0300

    OY-5547 Poistetaan autentikoitumaton hyvaksynnan-ehto rajapinta

--- valinta-tulos-service/src/main/scala/ScalatraBootstrap.scala
+++ valinta-tulos-service/src/main/scala/ScalatraBootstrap.scala
@@ -160,7 +160,6 @@ class ScalatraBootstrap extends LifeCycle with Logging {
       context.mount(new HakijanVastaanottoServlet(vastaanottoService), "/vastaanotto", "vastaanotto")
       context.mount(new ErillishakuServlet(valinnantulosService, hyvaksymiskirjeService, valintarekisteriDb, appConfig), "/erillishaku/valinnan-tulos", "erillishaku/valinnan-tulos")
       context.mount(new NoAuthSijoitteluServlet(sijoitteluService), "/sijoittelu", "sijoittelu")
-      context.mount(new NoAuthHyvaksynnanEhtoServlet(valintarekisteriDb), "/hyvaksynnan-ehto", "hyvaksynnan-ehto")
 
       context.mount(new SiirtotiedostoServlet(siirtotiedostoService, valintarekisteriDb), "/cas/siirtotiedosto", "siirtotiedosto")
 
