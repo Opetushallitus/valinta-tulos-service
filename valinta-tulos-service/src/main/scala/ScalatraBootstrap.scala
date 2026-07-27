cd3a242760af77229ce727e159e54c63dd2b6491
Topias Heinonen <topias.heinonen@gofore.com>
2026-07-27 09:05:34 +0300

    OY-5547 Poistetaan autentikoitumaton sijoittelu-rajapinta

--- valinta-tulos-service/src/main/scala/ScalatraBootstrap.scala
+++ valinta-tulos-service/src/main/scala/ScalatraBootstrap.scala
@@ -159,7 +159,6 @@ class ScalatraBootstrap extends LifeCycle with Logging {
       context.mount(new EnsikertalaisuusServlet(valintarekisteriDb, appConfig.settings.valintaRekisteriEnsikertalaisuusMaxPersonOids), "/ensikertalaisuus", "ensikertalaisuus")
       context.mount(new HakijanVastaanottoServlet(vastaanottoService), "/vastaanotto", "vastaanotto")
       context.mount(new ErillishakuServlet(valinnantulosService, hyvaksymiskirjeService, valintarekisteriDb, appConfig), "/erillishaku/valinnan-tulos", "erillishaku/valinnan-tulos")
-      context.mount(new NoAuthSijoitteluServlet(sijoitteluService), "/sijoittelu", "sijoittelu")
 
       context.mount(new SiirtotiedostoServlet(siirtotiedostoService, valintarekisteriDb), "/cas/siirtotiedosto", "siirtotiedosto")
 
