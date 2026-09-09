0e926b72ec59938a114a7d6820ed961e7027f173
Topias Heinonen <topias.heinonen@gofore.com>
2026-09-09 16:59:08 +0300

    OY-5811 Autentikoidut versiot koostepalvelun lukuvuosi-rajapinnoille
    Lisätään autentikoidut versiot valintalaskentakoostepalvelun käyttämille
    lukuvuosimaksu-rajapinnoille.
    
    Lukurajapinta on /auth/lukuvuosimaksut/read/bulk, koska pelkkä /read
    törmäisi hakukohdeoid-rajapinnan kanssa.
    
    Kirjoitusrajapinta on /auth/lukuvuosimaksut/write/:hakukohdeOid, ja se
    eroaa olemassaolevasta lähinnä sillä, että se lukee auditlogille
    kirjoitettavat tiedot pyynnön vartalosta.
    
    Molemmat ovat selvästi sisäisiä rajapintoja, joten niiden käyttämiseen
    pitää olla VALINTATULOSSERVICE_CRUD-oikeus OPH:n organisaatiolla. Tämän
    takia ei tarvita myöskään organisaatiotarkistuksia, kun kutsujalla on
    aina oikeus kaikkiin organisaatioihin.

--- valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/local/LukuvuosimaksuServletSpec.scala
+++ valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/local/LukuvuosimaksuServletSpec.scala
@@ -1,14 +1,14 @@
 package fi.vm.sade.valintatulosservice.local
 
-import java.util.Date
-
 import fi.vm.sade.valintatulosservice._
 import fi.vm.sade.valintatulosservice.config.VtsAppConfig
 import fi.vm.sade.valintatulosservice.lukuvuosimaksut.LukuvuosimaksuMuutos
+import fi.vm.sade.valintatulosservice.security.Role
 import fi.vm.sade.valintatulosservice.valintarekisteri.ValintarekisteriDbTools
 import fi.vm.sade.valintatulosservice.valintarekisteri.domain._
-import org.json4s.{DefaultFormats, Formats}
+import org.json4s.native.JsonMethods.parse
 import org.json4s.native.JsonParser
+import org.json4s.{DefaultFormats, Formats}
 import org.junit.runner.RunWith
 import org.mockserver.integration.ClientAndServer
 import org.mockserver.model.{HttpRequest, HttpResponse}
@@ -29,11 +29,14 @@ class LukuvuosimaksuServletSpec extends ServletSpecification with Valintarekiste
   lazy val vapautettu = LukuvuosimaksuMuutos("1.2.3.personOid", Maksuntila.vapautettu)
   lazy val maksettu = LukuvuosimaksuMuutos("1.2.3.personOid", Maksuntila.maksettu)
   lazy val testSession = createTestSession()
+  lazy val wrongRolesSession = createTestSession(Set(Role.ATARU_KEVYT_VALINTA_READ))
+  lazy val ophSession = createTestSession(Set(Role.VALINTATULOSSERVICE_CRUD_OPH))
   lazy val auditSession: AuditSessionRequest = AuditSessionRequest("1.2.3.4", List(), "userAgent", "localhost")
 
-
   private val httpHeaders: Map[String, String] = Map("Content-type" -> "application/json")
-  private lazy val httpHeadersWithSession: Map[String, String] = Map("Cookie" -> s"session=${testSession}", "Content-type" -> "application/json")
+  private lazy val httpHeadersWithSession: Map[String, String] = Map("Cookie" -> s"session=$testSession", "Content-type" -> "application/json")
+
+  private def headers(auditSession: String): Map[String, String] = Map("Cookie" -> s"session=$auditSession", "Content-type" -> "application/json")
 
   "Lukuvuosimaksu API without CAS should work" should {
     "palauttaa 204 when POST with 'auditInfo'" in {
@@ -44,6 +47,7 @@ class LukuvuosimaksuServletSpec extends ServletSpecification with Valintarekiste
     "fail with 400 when calling without 'auditInfo'" in {
       post(s"lukuvuosimaksu/write/1.2.3.200", muutosAsJson(vapautettu), httpHeaders) {
         status must_== 400
+        (parse(body) \ "error").extract[String] must startWith("No usable value for auditSession")
       }
     }
     "find lukuvuosimaksut by several hakukohde oids" in {
@@ -65,7 +69,106 @@ class LukuvuosimaksuServletSpec extends ServletSpecification with Valintarekiste
     }
   }
 
-  "POST /auth/lukuvuosimaksu" should {
+  "POST /auth/lukuvuosimaksu/read/bulk" should {
+    val url = "auth/lukuvuosimaksu/read/bulk"
+    "palauttaa 401 ilman sessiota" in {
+      post(url, bulkReadRequest(HakukohdeOid("1.2.3.200")), httpHeaders) {
+        status must_== 401
+        body must_== """{"error":"Unauthenticated: No session found"}"""
+      }
+    }
+
+    "palauttaa 403 puutteellisilla oikeuksilla" in {
+      post(url, bulkReadRequest(HakukohdeOid("1.2.3.200")), httpHeadersWithSession) {
+        status must_== 403
+        body must_== """{"error":"Forbidden: null"}"""
+      }
+    }
+
+    "palauttaa 400 ilman audit-infoa" in {
+      val request = """{"hakukohdeOids":["1.2.3.200"]}""".getBytes("UTF-8")
+      post(url, request, headers(ophSession)) {
+        status must_== 400
+
+        (parse(body) \ "error").extract[String] must startWith("No usable value for auditSession")
+      }
+    }
+
+    "palauttaa 200 ja tyhjän listan, kun lukuvuosimaksuja ei ole" in {
+      post(url, bulkReadRequest(HakukohdeOid("1.2.3.404")), headers(ophSession)) {
+        status must_== 200
+        body must_== """[]"""
+      }
+    }
+
+    "löytää talletetun maksun" in {
+      val maksettavaKohde = HakukohdeOid("1.2.3.200")
+      post(s"auth/lukuvuosimaksu/write/${maksettavaKohde.s}", muutosAsJsonWithAuditSession(maksettu), headers(ophSession)) {
+        status must_== 204
+      }
+
+      post(url, serialiseToJson(LukuvuosimaksuBulkReadRequest(List(maksettavaKohde, HakukohdeOid("1.2.3.300")), auditSession)), headers(ophSession)) {
+        val maksut = JsonParser.parse(body).extract[Seq[Lukuvuosimaksu]]
+        maksut must have size 1
+        val maksu = maksut.head
+        maksu.personOid must_== maksettu.personOid
+        maksu.maksuntila must_== maksettu.maksuntila
+        maksu.hakukohdeOid must_== maksettavaKohde
+        maksu.muokkaaja must_== auditSession.personOid
+        maksu.luotu.getTime must be_< (System.currentTimeMillis() + (60 * 1000))
+        status must_== 200
+      }
+
+    }
+  }
+
+  "POST /auth/lukuvuosimaksu/write/:hakukohdeOid" should {
+    val maksettavaKohde = HakukohdeOid("1.2.3.200")
+    val url = s"auth/lukuvuosimaksu/write/${maksettavaKohde.s}"
+
+    "palauttaa 401 ilman sessiota" in {
+      post(url, bulkReadRequest(HakukohdeOid("1.2.3.200")), httpHeaders) {
+        status must_== 401
+        body must_== """{"error":"Unauthenticated: No session found"}"""
+      }
+    }
+
+    "palauttaa 403 puutteellisilla oikeuksilla" in {
+      post(url, bulkReadRequest(HakukohdeOid("1.2.3.200")), httpHeadersWithSession) {
+        status must_== 403
+        body must_== """{"error":"Forbidden: null"}"""
+      }
+    }
+
+    "palauttaa 400 ilman audit-infoa" in {
+      val request = """{"hakukohdeOids":["1.2.3.200"]}""".getBytes("UTF-8")
+      post(url, request, headers(ophSession)) {
+        status must_== 400
+
+        (parse(body) \ "error").extract[String] must startWith("No usable value for auditSession")
+      }
+    }
+
+    "palauttaa 204 kun maksut talletettu" in {
+      post(s"lukuvuosimaksu/write/${maksettavaKohde.s}", muutosAsJsonWithAuditSession(maksettu), headers(ophSession)) {
+        status must_== 204
+      }
+    }
+  }
+
+  "POST /auth/lukuvuosimaksu/:hakukohdeOid" should {
+    "palauttaa 401 ilman sessiota" in {
+      post(s"auth/lukuvuosimaksu/1.2.3.100", muutosAsJson(vapautettu), httpHeaders) {
+        status must_== 401
+      }
+    }
+
+    "palauttaa 403 puutteellisilla oikeuksilla" in {
+      post(s"auth/lukuvuosimaksu/1.2.3.100", muutosAsJson(vapautettu), headers(wrongRolesSession)) {
+        status must_== 403
+      }
+    }
+
     "palauttaa 204 kun tallennus onnistuu" in {
       post(s"auth/lukuvuosimaksu/1.2.3.100", muutosAsJson(vapautettu), httpHeadersWithSession) {
         status must_== 204
@@ -97,6 +200,10 @@ class LukuvuosimaksuServletSpec extends ServletSpecification with Valintarekiste
 
   private def muutosAsJsonWithAuditSession(l: LukuvuosimaksuMuutos) = {
     serialiseToJson(LukuvuosimaksuRequest(List(l), auditSession))
+  }
+
+  private def bulkReadRequest(hakukohdeOid: HakukohdeOid, auditSession: AuditSessionRequest = auditSession) = {
+    serialiseToJson(LukuvuosimaksuBulkReadRequest(List(hakukohdeOid), auditSession))
   }
 
   private def muutosAsJson(l: LukuvuosimaksuMuutos) = serialiseToJson(List(l))
